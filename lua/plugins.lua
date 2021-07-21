b7c9cd4c51d7e4540d3605a6bfecdb22edb38cea
mcaisey <mcaisey@thoughtmachine.net>
2021-07-21 15:01:36 +0100

    disable yaml highlighting from tree-sitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -87,6 +87,7 @@ require('nvim-treesitter.configs').setup {
   ensure_installed = 'maintained',
   highlight = {
     enable = true,
+    disable = {'yaml'},
   },
   incremental_selection = {
     enable = true,
