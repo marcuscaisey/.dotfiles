433522af2ef0766374f551e61b8430388f99567e
mcaisey <mcaisey@thoughtmachine.net>
2021-07-21 15:01:52 +0100

    use single quotes for tree-sitter keymaps

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -92,10 +92,10 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
     },
   },
   indent = {
