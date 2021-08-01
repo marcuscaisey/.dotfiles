f85183f4acba0107ace939469a8b777e42428452
Marcus Caisey <marcus@teckna.com>
2021-08-01 16:43:26 +0100

    extract echo into utils.sh

--- utils.sh
+++ utils.sh
@@ -1,7 +1,14 @@
+# Return if we're running on osx
 osx() {
   [ $(uname) = "Darwin" ]
 }
 
+# Return if we're running on osx
 linux() {
   [ $(uname) = "Linux" ]
+}
+
+# echo wrapper which cycles between output colours on subsequent calls
+cecho() {
+  echo "$(tput setaf ${ci:-1})$1$(tput sgr0)" && ci=$(( (${ci:-1} % 6) + 1 ))
 }
