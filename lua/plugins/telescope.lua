aa025baf9babcf5fc980919fc9257a9e01208374
Marcus Caisey <marcus@teckna.com>
2021-08-15 18:21:27 +0100

    more telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,13 +12,24 @@ telescope.setup {
     selection_caret = '  ',
   },
   pickers = {
+    find_files = {
+      theme = 'dropdown',
+      previewer = false,
+    },
+    buffers = {
+      theme = 'dropdown',
+      previewer = false,
+    },
     current_buffer_fuzzy_find = {
       previewer = false,
       sorting_strategy = 'ascending',
     },
     lsp_document_symbols = {
       theme = 'dropdown',
-    }
+    },
+    lsp_references = {
+      theme = 'dropdown',
+    },
   },
   extensions = {
     fzf = {
