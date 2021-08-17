12d6415bcac1712bf0361859f715646ca2514728
mcaisey <mcaisey@thoughtmachine.net>
2021-08-17 16:16:30 +0100

    configure gopls a bit more

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -1,4 +1,13 @@
 local lspconfig = require 'lspconfig'
 
-lspconfig.gopls.setup{}
+lspconfig.gopls.setup{
+  flags = {
+    debounce_text_changes = 500,
+  },
+  settings = {
+    gopls = {
+      directoryFilters = {'-plz-out'},
+    },
+  },
+}
 lspconfig.pyright.setup{}
