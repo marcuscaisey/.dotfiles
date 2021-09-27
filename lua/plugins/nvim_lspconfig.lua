ea4e44c12053cb499d375789b3d55049679166df
Marcus Caisey <marcus@teckna.com>
2021-09-27 17:49:25 +0100

    highlight errors and warnings properly

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -1,4 +1,6 @@
 local lspconfig = require 'lspconfig'
+local fn = vim.fn
+local highlight = vim.highlight
 
 lspconfig.gopls.setup{
   flags = {
@@ -11,3 +13,11 @@ lspconfig.gopls.setup{
   },
 }
 lspconfig.pyright.setup{}
+
+-- Use icons in theme colour for error / warning signs
+fn.sign_define('LspDiagnosticsSignError', {text = '', texthl = 'LspDiagnosticsError'})
+fn.sign_define('LspDiagnosticsSignWarning', {text = '', texthl = 'LspDiagnosticsWarning'})
+
+-- LspDiagnosticsError and LspDiagnosticsWarning are linked to theme groups so link default groups to them as well
+highlight.link('LspDiagnosticsDefaultError', 'LspDiagnosticsError', true)
+highlight.link('LspDiagnosticsDefaultWarning', 'LspDiagnosticsWarning', true)
