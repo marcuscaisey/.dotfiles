9f29c54a5221d2aea5fd58673d2df0ae070013bd
Marcus Caisey <marcus@teckna.com>
2021-09-27 22:44:28 +0100

    move some nvim-tree config into setup func

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,14 +1,21 @@
 local g = vim.g
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
-g.nvim_tree_bindings = {
-  {key = 'h', cb = tree_cb('close_node')},
-  {key = 'l', cb = tree_cb("edit")},
+require('nvim-tree').setup{
+  auto_close = true,
+  follow = true,
+  view = {
+    mappings = {
+      list = {
+        {key = 'h', cb = tree_cb('close_node')},
+        {key = 'l', cb = tree_cb("edit")},
+      },
+    },
+  },
 }
+
 g.nvim_tree_ignore = {'.git'}
-g.nvim_tree_auto_close = 1
 g.nvim_tree_quit_on_open = 1
-g.nvim_tree_follow = 1
 g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
