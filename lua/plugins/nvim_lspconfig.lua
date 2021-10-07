96ed95db154d5266f588c5322645a8ba15b94c77
Marcus Caisey <marcus@teckna.com>
2021-10-07 14:29:56 +0100

    remove unneeded root_dir config from pyright config

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -22,18 +22,6 @@ lspconfig.pyright.setup{
   flags = {
     debounce_text_changes = 150,
   },
-  root_dir = function(fname)
-    local root_files = {
-      'pyproject.toml',
-      'setup.py',
-      'setup.cfg',
-      'requirements.txt',
-      'Pipfile',
-      'pyrightconfig.json',
-      'BUILD',
-    }
-    return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
-  end,
   settings = {
     python = {
       analysis = {
