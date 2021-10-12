39d6993ca1dae0246a61e89e92717bdf362c2222
mcaisey <mcaisey@thoughtmachine.net>
2021-10-12 23:30:31 +0100

    disable quit nvim-tree on open

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,6 +1,12 @@
 local g = vim.g
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
+g.nvim_tree_ignore = {'.git'}
+g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_highlight_opened_files = 1
+g.nvim_tree_group_empty = 1
+
 require('nvim-tree').setup{
   auto_close = true,
   follow = true,
@@ -13,10 +19,3 @@ require('nvim-tree').setup{
     },
   },
 }
-
-g.nvim_tree_ignore = {'.git'}
-g.nvim_tree_quit_on_open = 1
-g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_group_empty = 1
