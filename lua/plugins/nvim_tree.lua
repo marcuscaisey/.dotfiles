78b599a2fb354ee91d9c22eb5066ab9728762be9
mcaisey <mcaisey@thoughtmachine.net>
2021-10-14 15:48:48 +0100

    enable nvim tree quit on open again

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -3,6 +3,7 @@ local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
 g.nvim_tree_ignore = {'.git'}
 g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_quit_on_open = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_group_empty = 1
