8a34e85ad4104f5597f3e3ad9dceaa4ae4dbd081
mcaisey <mcaisey@thoughtmachine.net>
2021-11-02 10:43:16 +0000

    move nvim_tree_ignore and nvim_tree_hide_dotfiles to setup func

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,8 +1,6 @@
 local g = vim.g
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
-g.nvim_tree_ignore = {'.git'}
-g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_quit_on_open = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
@@ -10,8 +8,13 @@ g.nvim_tree_group_empty = 1
 
 require('nvim-tree').setup{
   auto_close = true,
+  hide_dotfiles = true,
+  ignore = {'.git'},
   update_focused_file = {
     enable = true,
+  },
+  filters = {
+    dotfiles = true,
   },
   view = {
     width = '25%',
