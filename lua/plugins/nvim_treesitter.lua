6ec9fbc345a5ad3fca15c1abddd41f12b58a007a
mcaisey <mcaisey@thoughtmachine.net>
2021-11-08 17:03:51 +0000

    change treesitter init_selection to grn

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -10,7 +10,7 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = 'gnn',
+      init_selection = 'grn',
       node_incremental = 'grn',
       scope_incremental = 'grc',
       node_decremental = 'grm',
