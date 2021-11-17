174bf7803fd447931708a676c9a09bb756d6a807
mcaisey <mcaisey@thoughtmachine.net>
2021-11-17 23:01:30 +0000

    do formatting of go with neoformat instead of lsp

--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -1,2 +1,9 @@
 local g = vim.g
+local cmd = vim.cmd
 g.neoformat_enabled_python = {'black'}
+
+-- Autoformat go files on save
+cmd([[autocmd BufWritePre *.go Neoformat gofmt]])
+
+-- Organise go imports on save
+cmd([[autocmd BufWritePre *.go Neoformat goimports]])
--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -3,7 +3,6 @@ local configs = require 'lspconfig/configs'
 local util = require 'lspconfig.util'
 local fn = vim.fn
 local highlight = vim.highlight
-local cmd = vim.cmd
 local api = vim.api
 
 lspconfig.gopls.setup{
@@ -115,40 +114,3 @@ fn.sign_define('LspDiagnosticsSignHint', {text = '', texthl = 'LspDiagnostics
 highlight.link('LspDiagnosticsDefaultError', 'LspDiagnosticsError', true)
 highlight.link('LspDiagnosticsDefaultWarning', 'LspDiagnosticsWarning', true)
 highlight.link('LspDiagnosticsDefaultHint', 'LspDiagnosticsHint', true)
-
--- Autoformat go files on save
-cmd([[autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)]])
-
--- Organise go imports on save
-cmd([[autocmd BufWritePre *.go lua goimports(1000)]])
-
--- copied from https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
-function goimports(timeout_ms)
-  local context = { only = { "source.organizeImports" } }
-  vim.validate { context = { context, "t", true } }
-
-  local params = vim.lsp.util.make_range_params()
-  params.context = context
-
-  -- See the implementation of the textDocument/codeAction callback
-  -- (lua/vim/lsp/handler.lua) for how to do this properly.
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-  if not result or next(result) == nil then return end
-  local actions = result[1].result
-  if not actions then return end
-  local action = actions[1]
-
-  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-  -- is a CodeAction, it can have either an edit, a command or both. Edits
-  -- should be executed first.
-  if action.edit or type(action.command) == "table" then
-    if action.edit then
-      vim.lsp.util.apply_workspace_edit(action.edit)
-    end
-    if type(action.command) == "table" then
-      vim.lsp.buf.execute_command(action.command)
-    end
-  else
-    vim.lsp.buf.execute_command(action)
-  end
-end
