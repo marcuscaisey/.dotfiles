f8efd65ef92b154c97ed84813aef1cedb9038ca7
mcaisey <mcaisey@thoughtmachine.net>
2021-11-18 15:06:17 +0000

    do neoformatting silently

--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -3,7 +3,7 @@ local cmd = vim.cmd
 g.neoformat_enabled_python = {'black'}
 
 -- Autoformat go files on save
-cmd([[autocmd BufWritePre *.go Neoformat gofmt]])
+cmd([[autocmd BufWritePre *.go silent Neoformat gofmt]])
 
 -- Organise go imports on save
-cmd([[autocmd BufWritePre *.go Neoformat goimports]])
+cmd([[autocmd BufWritePre *.go silent Neoformat goimports]])
