e82b753e6f8152eb3521e6e80658f33aebee703a
Marcus Caisey <marcus@teckna.com>
2021-12-24 17:19:37 +0000

    cleanup autoformatting config

--- nvim/lua/plugins/neoformat.lua
+++ nvim/lua/plugins/neoformat.lua
@@ -1,20 +1,27 @@
 local g = vim.g
 local cmd = vim.cmd
 
-g.neoformat_enabled_python = {'black'}
+-- Format all files with extensions defined here on save
+local autoformatted_extensions = {'go', 'lua'}
 
+for k, v in pairs(autoformatted_extensions) do autoformatted_extensions[k] = '*.' .. v end
+cmd(string.format([[
+  augroup auto_neoformat
+    autocmd!
+    autocmd BufWritePre %s undojoin | silent Neoformat
+  augroup END
+]], table.concat(autoformatted_extensions, ',')))
+
+g.neoformat_enabled_python = {'black'}
 g.neoformat_enabled_lua = {'luaformat'}
+g.neoformat_enabled_go = {'gofmt', 'goimports'}
 
 g.neoformat_lua_luaformat = {
   exe = 'lua-format',
-  args = {'--indent-width', '2', '--double-quote-to-single-quote'}
+  args = {'--column-limit', '120', '--indent-width', '2', '--double-quote-to-single-quote'}
 }
 
--- Autoformat go files on save
-cmd('autocmd BufWritePre *.go silent Neoformat gofmt')
+g.neoformat_run_all_formatters = true
 
--- Organise go imports on save
-cmd('autocmd BufWritePre *.go silent Neoformat goimports')
 
--- Autoformat lua files on save
-cmd('autocmd BufWritePre *.lua silent Neoformat')
+
