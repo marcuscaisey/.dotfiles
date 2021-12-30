fdcd454f7918a6f1df479ceeb958221275f16159
Marcus Caisey <marcus@teckna.com>
2021-12-30 18:18:26 +0000

    make nvim-tree cd change nvim pwd as well

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -1,5 +1,7 @@
 local g = vim.g
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
+local nvim_tree = require 'nvim-tree'
+local cmd = vim.cmd
 
 g.nvim_tree_quit_on_open = 1
 g.nvim_tree_git_hl = 1
@@ -7,7 +9,7 @@ g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_group_empty = 1
 g.nvim_tree_respect_buf_cwd = 1
 
-require('nvim-tree').setup {
+nvim_tree.setup {
   auto_close = true,
   hide_dotfiles = true,
   ignore = { '.git' },
@@ -29,7 +31,17 @@ require('nvim-tree').setup {
           key = 'l',
           cb = tree_cb 'edit',
         },
+        {
+          key = '<c-]>',
+          cb = ':lua NvimTreeCD() <cr>',
+        },
       },
     },
   },
 }
+
+--- Changes the NvimTree cwd and changes the nvim cwd to that as well
+function NvimTreeCD()
+  nvim_tree.on_keypress 'cd'
+  cmd('cd ' .. require('nvim-tree.lib').Tree.cwd)
+end
