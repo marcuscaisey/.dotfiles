bf2f4ac9d51d9b9f8f4682a4e3b37f3cacd3eedf
mcaisey <mcaisey@thoughtmachine.net>
2022-01-18 00:56:08 +0000

    move filetype autocmds from misc.lua to filetypes.lua

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -1,3 +1,27 @@
 -- Only use lua filetype detection
 vim.g.do_filetype_lua = 1
 vim.g.did_load_filetypes = 0
+
+vim.filetype.add {
+  extension = {
+    build_defs = 'please',
+    plz = 'please',
+  },
+  filename = {
+    ['BUILD'] = 'please',
+  },
+}
+
+local two_space_tab_types = {
+  'lua',
+  'javascript',
+}
+vim.cmd(string.format(
+  [[
+  augroup two_space_tabs
+    autocmd!
+    autocmd FileType %s set tabstop=2 shiftwidth=2
+  augroup END
+]],
+  table.concat(two_space_tab_types, ',')
+))
--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -4,22 +4,13 @@ vim.cmd [[autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") |
 -- Trim trailing whitespace on write
 vim.cmd [[autocmd BufWritePre * if expand('%:e') !=# 'diff' | %s/\s\+$//e | endif]]
 
--- Use 2 space tabs for lua
-vim.cmd 'autocmd FileType lua,javascript set tabstop=2 shiftwidth=2'
-
--- Set file type as bazel for build_defs files
-vim.cmd 'autocmd BufNewFile,BufRead *.build_defs set filetype=bzl'
-
 -- Don't comment lines after o/O
 vim.cmd 'autocmd BufEnter * set formatoptions-=o'
 
--- Set filetype as please in BUILD files
-vim.cmd 'autocmd BufRead,BufNewFile BUILD,*.build_defs,*.build_def,*.build set filetype=please'
-
 -- Highlight yanked text
 vim.cmd [[
-augroup highlight_yank
-  autocmd!
-  au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=1000 }
-augroup END
+  augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=1000 }
+  augroup END
 ]]
