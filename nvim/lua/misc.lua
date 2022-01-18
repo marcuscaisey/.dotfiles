ba173c5680d4efd1b7f6538fcd57e4ef320a945d
mcaisey <mcaisey@thoughtmachine.net>
2022-01-18 01:31:17 +0000

    tidy up misc autocmds

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -1,16 +1,9 @@
+vim.cmd 'augroup misc'
+vim.cmd '  autocmd!'
 -- Jump to last position when opening a file
-vim.cmd [[autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]]
-
+vim.cmd [[ autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]]
 -- Trim trailing whitespace on write
-vim.cmd [[autocmd BufWritePre * if expand('%:e') !=# 'diff' | %s/\s\+$//e | endif]]
-
--- Don't comment lines after o/O
-vim.cmd 'autocmd BufEnter * set formatoptions-=o'
-
+vim.cmd [[ autocmd BufWritePre * if expand('%:e') !=# 'diff' | %s/\s\+$//e | endif]]
 -- Highlight yanked text
-vim.cmd [[
-  augroup highlight_yank
-    autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=1000 }
-  augroup END
-]]
+vim.cmd [[ autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=1000 }]]
+vim.cmd 'augroup END'
