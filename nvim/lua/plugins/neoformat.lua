45031851e5087848680fff30faeee774692272c6
mcaisey <mcaisey@thoughtmachine.net>
2022-01-18 01:29:05 +0000

    cleanup auto-neoformatting autocmd

--- nvim/lua/plugins/neoformat.lua
+++ nvim/lua/plugins/neoformat.lua
@@ -1,31 +1,22 @@
--- Format all files with extensions defined here on save
-local autoformatted_extensions = {
-  'go',
-  'lua',
-  'py',
-}
-
-for k, v in pairs(autoformatted_extensions) do
-  autoformatted_extensions[k] = '*.' .. v
-end
-vim.cmd(string.format(
-  [[
-  augroup auto_neoformat
-    autocmd!
-    autocmd BufWritePre %s silent lua AutoNeoformat()
-  augroup END
-]],
-  table.concat(autoformatted_extensions, ',')
-))
-
 vim.g.neoformat_enabled_python = { 'black' }
 vim.g.neoformat_enabled_lua = { 'stylua' }
 vim.g.neoformat_enabled_go = { 'goimports' }
 
+-- Format these filetypes on save
+local autoformatted_filetypes = {
+  go = true,
+  lua = true,
+  python = true,
+}
+
+vim.cmd 'augroup auto_neoformat'
+vim.cmd '  autocmd!'
+vim.cmd '  autocmd BufWritePre * silent lua AutoNeoformat()'
+vim.cmd 'augroup END'
+
 local auto_formatting_enabled = true
 
---- Toggles Neoformat auto-formatting for filetypes which configured to be
--- formatted on save.
+--- Toggles Neoformat auto-formatting for filetypes which configured to be formatted on save.
 local function toggle_auto_neoformatting()
   if auto_formatting_enabled then
     auto_formatting_enabled = false
@@ -36,11 +27,12 @@ local function toggle_auto_neoformatting()
   end
 end
 
---- Wrapper around the vimscript Neoformat command which only runs Neoformat if
--- auto-formatting is currently enabled. Auto-formatting can be toggled by
--- calling ToggleAutoNeoformatting().
+--- Wrapper around the vimscript Neoformat command which only runs Neoformat if the filetype is in -
+--- autoformatted_filetypes and auto-formatting is currently enabled. Auto-formatting can be toggled by - calling
+--- ToggleAutoNeoformatting().
 function AutoNeoformat()
-  if auto_formatting_enabled then
+  print(autoformatted_filetypes[vim.o.filetype], auto_formatting_enabled)
+  if autoformatted_filetypes[vim.o.filetype] and auto_formatting_enabled then
     vim.cmd 'Neoformat'
   end
 end
