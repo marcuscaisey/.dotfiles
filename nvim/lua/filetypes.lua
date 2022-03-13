7ab556138305082f11aabce3f0ed79194dabcb4c
Marcus Caisey <marcus@teckna.com>
2022-03-13 19:54:49 +0000

    extract out custom filetype setup helper

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -1,52 +1,54 @@
--- Only use lua filetype detection
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
+local augroup = vim.api.nvim_create_augroup('per_file_type_settings', { clear = true })
 
-vim.filetype.add {
-  extension = {
-    build_defs = 'please',
-    plz = 'please',
-  },
-  filename = {
-    ['BUILD'] = 'please',
-  },
+local function add_tab_size_autocmd(file_type, tab_size)
+  vim.api.nvim_create_autocmd('FileType', {
+    callback = function()
+      vim.bo.tabstop = tab_size
+      vim.bo.shiftwidth = tab_size
+    end,
+    pattern = file_type,
+    group = augroup,
+    desc = string.format('Use %d space tabs', tab_size),
+  })
+end
+
+local function add_text_width_autocmd(file_type, text_width)
+  vim.api.nvim_create_autocmd('FileType', {
+    callback = function()
+      vim.bo.textwidth = text_width
+      vim.cmd 'setlocal formatoptions-=o'
+    end,
+    pattern = file_type,
+    group = augroup,
+    desc = string.format('Use %d textwidth', text_width),
+  })
+end
+
+local function add_indent_with_tabs_autocmd(file_type)
+  vim.api.nvim_create_autocmd('FileType', {
+    callback = function()
+      vim.bo.expandtab = false
+    end,
+    pattern = file_type,
+    group = augroup,
+    desc = 'Indent with tabs',
+  })
+end
+
+local function setup(opts)
+  for file_type, settings in pairs(opts) do
+    if settings.tab_size then
+      add_tab_size_autocmd(file_type, settings.tab_size)
+    end
+    if settings.text_width then
+      add_text_width_autocmd(file_type, settings.text_width)
+    end
+    if settings.indent_with_tabs then
+      add_indent_with_tabs_autocmd(file_type)
+    end
+  end
+end
+
+return {
+  setup = setup,
 }
-
-local group = vim.api.nvim_create_augroup('filetypes', { clear = true })
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.bo.tabstop = 2
-    vim.bo.shiftwidth = 2
-  end,
-  pattern = { 'lua', 'javascript', 'json', 'jsonc' },
-  group = group,
-  desc = 'Use 2 space tabs',
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.bo.textwidth = 100
-  end,
-  pattern = { 'python' },
-  group = group,
-  desc = 'Use 100 textwidth',
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.bo.textwidth = 120
-  end,
-  pattern = { 'go', 'lua' },
-  group = group,
-  desc = 'Use 120 textwidth',
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.bo.expandtab = false
-  end,
-  pattern = { 'go' },
-  group = group,
-  desc = 'Indent with tabs',
-})
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -21,3 +21,38 @@ vim.o.swapfile = false
 vim.o.tabstop = 4
 vim.o.termguicolors = true
 vim.o.updatetime = 100
+
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
+vim.filetype.add {
+  extension = {
+    build_defs = 'please',
+    plz = 'please',
+  },
+  filename = {
+    ['BUILD'] = 'please',
+  },
+}
+
+require('filetypes').setup {
+  lua = {
+    tab_size = 2,
+    text_width = 120,
+  },
+  javascript = {
+    tab_size = 2,
+  },
+  json = {
+    tab_size = 2,
+  },
+  jsonc = {
+    tab_size = 2,
+  },
+  python = {
+    text_width = 100,
+  },
+  go = {
+    text_width = 120,
+    indent_with_tabs = true,
+  },
+}
