3f974c9ae86437e3929777062f189c8eaf78616f
Marcus Caisey <marcus@teckna.com>
2022-03-13 18:16:31 +0000

    remove find in folder map

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,14 +4,3 @@ require 'filetypes'
 require 'colours'
 require 'misc'
 require 'plugins.packer'
-
-vim.keymap.set('n', '<leader>ff', function()
-  local dirs = vim.fn.systemlist 'fd --type directory --strip-cwd-prefix'
-  vim.ui.select(dirs, { prompt = 'Find files in:' }, function(selection)
-    require('telescope.builtin').find_files { cwd = selection }
-    -- hack to make the prompt start in insert mode
-    vim.defer_fn(function()
-      vim.cmd 'startinsert'
-    end, 0)
-  end)
-end)
