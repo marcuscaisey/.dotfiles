6ec931ac3f04e66d79016ac527b3aded5e40142b
Marcus Caisey <marcus@teckna.com>
2022-03-13 14:38:04 +0000

    rewrite commentary autocmds in lua

--- nvim/lua/plugins/commentary.lua
+++ nvim/lua/plugins/commentary.lua
@@ -1,13 +1,17 @@
 local group = vim.api.nvim_create_augroup('commentary', { clear = true })
 
 vim.api.nvim_create_autocmd('FileType', {
-  command = 'setlocal commentstring=--%s',
+  callback = function()
+    vim.bo.commentstring = '--%s'
+  end,
   pattern = { 'sql' },
   group = group,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  command = 'setlocal commentstring=#%s',
+  callback = function()
+    vim.bo.commentstring = '#%s'
+  end,
   pattern = { 'toml', 'please' },
   group = group,
 })
