abf978700e62c05e91cfc36d2fa3e636a81af87e
Marcus Caisey <marcus@teckna.com>
2022-03-13 23:17:37 +0000

    move cmp config back into a single file

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,83 @@
+local cmp = require 'cmp'
+
+local function replace_termcodes(s)
+  return vim.api.nvim_replace_termcodes(s, false, false, true)
+end
+
+local function feedkeys(keys, mode)
+  vim.api.nvim_feedkeys(replace_termcodes(keys), mode, true)
+end
+
+--- Check if we should tab out of a pair of brackets / quotes. Returns true if
+--- the next character is a:
+--- - closing bracket
+--- - quote and we're inside a pair of them
+--- @return boolean
+local function should_tab_out()
+  local brackets = {
+    [')'] = true,
+    [']'] = true,
+    ['}'] = true,
+  }
+  local quotes = {
+    ["'"] = true,
+    ['"'] = true,
+    ['`'] = true,
+  }
+
+  local line = vim.fn.getline '.'
+  local col = vim.fn.col '.'
+  local next_char = line:sub(col, col)
+
+  if quotes[next_char] then
+    local preceding_chars = line:sub(1, col - 1)
+    local num_preceding_quotes = select(2, preceding_chars:gsub(next_char, ''))
+    -- if odd number of preceding quotes, then we're currently inside a pair
+    return num_preceding_quotes % 2 == 1
+  end
+  return brackets[next_char] == true
+end
+cmp.setup {
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'nvim_lua' },
+  },
+  snippet = {
+    expand = function(args)
+      vim.fn['vsnip#anonymous'](args.body)
+    end,
+  },
+  completion = {
+    completeopt = 'menu,menuone,preview,noinsert',
+  },
+  confirmation = {
+    default_behavior = cmp.ConfirmBehavior.Replace,
+  },
+  mapping = {
+    ['<c-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<c-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<c-space>'] = cmp.mapping.complete(),
+    ['<tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.abort()
+      elseif should_tab_out() then
+        feedkeys('<right>', 'i')
+      else
+        fallback()
+      end
+    end,
+    ['<cr>'] = cmp.mapping.confirm { select = true },
+  },
+  formatting = {
+    format = require('lspkind').cmp_format {
+      mode = 'symbol',
+      preset = 'codicons',
+    },
+  },
+  sorting = {
+    comparators = { cmp.config.compare.sort_text },
+  },
+}
+
+cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
