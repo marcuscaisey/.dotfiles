e9b6b1e0f7a4d7728d723d6e0fe040b7e253cd78
Marcus Caisey <marcus@teckna.com>
2022-03-13 18:45:29 +0000

    clean up commentary config

--- nvim/lua/plugins/commentary.lua
+++ nvim/lua/plugins/commentary.lua
@@ -1,17 +1,19 @@
+-- Set the comment characters for the given filetypes
+local comment_characters = {
+  sql = '--',
+  toml = '#',
+  please = '#',
+}
+
+-- Apply the comment characters for each filetype
 local group = vim.api.nvim_create_augroup('commentary', { clear = true })
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.bo.commentstring = '--%s'
-  end,
-  pattern = { 'sql' },
-  group = group,
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.bo.commentstring = '#%s'
-  end,
-  pattern = { 'toml', 'please' },
-  group = group,
-})
+for filetype, char in pairs(comment_characters) do
+  vim.api.nvim_create_autocmd('FileType', {
+    callback = function()
+      vim.bo.commentstring = char .. '%s'
+    end,
+    pattern = filetype,
+    group = group,
+    desc = string.format('Use %s for comments', char),
+  })
+end
