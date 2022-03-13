a457541b74b121e3604007f4018ff5218f8fffb3
Marcus Caisey <marcus@teckna.com>
2022-03-13 23:13:21 +0000

    remove bufferline nvim-tree offset

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -15,12 +15,5 @@ require('bufferline').setup {
     tab_size = 12,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    offsets = {
-      {
-        filetype = 'NvimTree',
-        highlight = 'Directory',
-        text_align = 'left',
-      },
-    },
   },
 }
