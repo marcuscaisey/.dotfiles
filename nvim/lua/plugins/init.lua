10580c75aad3b0581f40b9a4eae3b0cb1d9d20bd
Marcus Caisey <marcus@teckna.com>
2022-03-19 20:23:52 +0000

    use 1.x branch for neo-tree

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -100,7 +100,7 @@ require('packer').startup {
     }
     use {
       'nvim-neo-tree/neo-tree.nvim',
-      branch = 'main',
+      branch = '1.x',
       requires = {
         'nvim-lua/plenary.nvim',
         'kyazdani42/nvim-web-devicons',
