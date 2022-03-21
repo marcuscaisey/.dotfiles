6a5c1219d4947e95585d0f526ba28b64a5578f3b
mcaisey <mcaisey@thoughtmachine.net>
2022-03-21 09:55:56 +0000

    remove nosplit option set by default

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,7 +5,6 @@ vim.o.expandtab = true
 vim.o.hidden = true
 vim.o.hlsearch = false
 vim.o.ignorecase = true
-vim.o.inccommand = 'nosplit'
 vim.o.mouse = 'a'
 vim.o.number = true
 vim.o.pumheight = 10
