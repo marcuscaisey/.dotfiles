9901fa31a01d233f9effdd141794d56eb78b9c57
Marcus Caisey <marcus@teckna.com>
2022-04-05 23:28:19 +0100

    add target to end of lua func snippet

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -10,7 +10,7 @@ ls.config.setup {
 }
 
 ls.add_snippets('lua', {
-  s('func', fmt('function{}{}({})\n  {}\nend', { nonempty(1, ' ', ''), i(1), i(2), i(0) })),
+  s('func', fmt('function{}{}({})\n  {}\nend{}', { nonempty(1, ' ', ''), i(1), i(2), i(3), i(0) })),
   ls.parser.parse_snippet('lfunc', 'local ${1:name} = function($2)\n  $0\nend'),
   ls.parser.parse_snippet('mfunc', '${1:M}.${2:name} = function($3)\n  $0\nend'),
   ls.parser.parse_snippet('for', 'for ${1:k}, ${2:v} in pairs(${3:t}) do\n  $0\nend'),
