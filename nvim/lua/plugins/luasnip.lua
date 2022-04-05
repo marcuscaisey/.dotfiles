7e77f72b471cff1feb93e789f11374478508210f
Marcus Caisey <marcus@teckna.com>
2022-04-05 14:12:06 +0100

    add some basic lua snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -3,3 +3,14 @@ local ls = require 'luasnip'
 ls.config.setup {
   updateevents = 'TextChanged,TextChangedI',
 }
+
+ls.add_snippets('lua', {
+  ls.parser.parse_snippet('func', 'function ${1:name}($2)\n  $0\nend'),
+  ls.parser.parse_snippet('lfunc', 'local ${1:name} = function($2)\n  $0\nend'),
+  ls.parser.parse_snippet('mfunc', 'local ${1:M}.${1:name} = function($3)\n  $0\nend'),
+  ls.parser.parse_snippet('for', 'for ${1:k}, ${2:v} in pairs(${3:t}) do\n  $0\nend'),
+  ls.parser.parse_snippet('fori', 'for ${1:_}, ${2:v} in ipairs(${3:t}) do\n  $0\nend'),
+  ls.parser.parse_snippet('p', 'print($1)$0'),
+  ls.parser.parse_snippet('i', 'vim.inspect($1)$0'),
+  ls.parser.parse_snippet('pi', 'print(vim.inspect($1))$0'),
+}, { key = 'lua' })
