5533aa542c7610b61f32439b791eb25dd3d2cb46
Marcus Caisey <marcus@teckna.com>
2022-04-05 14:18:09 +0100

    fix mfunc lua snippet

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -7,7 +7,7 @@ ls.config.setup {
 ls.add_snippets('lua', {
   ls.parser.parse_snippet('func', 'function ${1:name}($2)\n  $0\nend'),
   ls.parser.parse_snippet('lfunc', 'local ${1:name} = function($2)\n  $0\nend'),
-  ls.parser.parse_snippet('mfunc', 'local ${1:M}.${1:name} = function($3)\n  $0\nend'),
+  ls.parser.parse_snippet('mfunc', 'local ${1:M}.${2:name} = function($3)\n  $0\nend'),
   ls.parser.parse_snippet('for', 'for ${1:k}, ${2:v} in pairs(${3:t}) do\n  $0\nend'),
   ls.parser.parse_snippet('fori', 'for ${1:_}, ${2:v} in ipairs(${3:t}) do\n  $0\nend'),
   ls.parser.parse_snippet('p', 'print($1)$0'),
