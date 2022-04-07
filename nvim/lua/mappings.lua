95e884244009a29ecea3235a9f638981e5978097
mcaisey <mcaisey@thoughtmachine.net>
2022-04-07 08:51:15 +0100

    test_target -> test_file

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -206,7 +206,7 @@ map('n', '<leader>lt', '<Plug>PlenaryTestFile')
 -- please.nvim
 map('n', '<leader>pj', please.jump_to_target)
 map('n', '<leader>pb', please.build_target)
-map('n', '<leader>pt', please.test_target)
+map('n', '<leader>pt', please.test_file)
 
 -- luasnip
 map({ 'i', 's' }, '<c-j>', function()
