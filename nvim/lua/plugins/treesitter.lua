11e28e71c46c08dbb9df7cc61674d82fc6bda3ee
Marcus Caisey <marcus@teckna.com>
2022-04-15 18:25:48 +0100

    add coment and markdown ts langs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.parsers').filetype_to_parsername.please = 'python'
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'lua', 'python', 'go', 'json', 'yaml', 'query' },
+  ensure_installed = { 'lua', 'python', 'go', 'json', 'yaml', 'query', 'comment', 'markdown' },
   highlight = {
     enable = true,
     disable = { 'yaml' },
