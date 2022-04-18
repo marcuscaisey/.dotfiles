04b1b13a376640ffc6eafbae2c89abe3695ac2ce
Marcus Caisey <marcus@teckna.com>
2022-04-18 19:28:19 +0100

    simplify lua func snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -24,9 +24,9 @@ ls.config.setup {
 }
 
 ls.add_snippets('lua', {
-  s('func', fmt('function{}{}({})\n  {}\nend{}', { nonempty(1, ' ', ''), i(1), i(2), i(3), i(0) })),
-  ls.parser.parse_snippet('lfunc', 'local ${1:name} = function($2)\n  $0\nend'),
-  ls.parser.parse_snippet('mfunc', '${1:M}.${2:name} = function($3)\n  $0\nend'),
+  ps('func', 'function($1)\n  $0\nend'),
+  ps('lfunc', 'local $1 = function($2)\n  $0\nend'),
+  ps('mfunc', '${1:M}.$2 = function($3)\n  $0\nend'),
   ls.parser.parse_snippet('for', 'for ${1:k}, ${2:v} in pairs(${3:t}) do\n  $0\nend'),
   ls.parser.parse_snippet('fori', 'for ${1:_}, ${2:v} in ipairs(${3:t}) do\n  $0\nend'),
   ls.parser.parse_snippet('while', 'while $1 do\n  $0\nend'),
