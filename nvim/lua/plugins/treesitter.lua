2ae3f6587c0c354dfdfa11e69406b2ef2c765f9c
Marcus Caisey <marcus@teckna.com>
2022-04-30 20:09:01 +0100

    enable vim treesitter parser

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.parsers').filetype_to_parsername.please = 'python'
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'lua', 'python', 'go', 'json', 'yaml', 'query', 'comment', 'markdown' },
+  ensure_installed = { 'lua', 'python', 'go', 'json', 'yaml', 'query', 'comment', 'markdown', 'vim' },
   highlight = {
     enable = true,
     disable = { 'yaml' },
