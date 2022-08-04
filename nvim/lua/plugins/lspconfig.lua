e47cfdc12dc08da93ef0dd57d548f2d92bed85a9
Marcus Caisey <marcus@teckna.com>
2022-08-04 16:49:27 +0100

    fix sumneko runtime files

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,7 +5,7 @@ local util = require 'lspconfig.util'
 local lua_runtime_path = vim.split(package.path, ';')
 table.insert(lua_runtime_path, 'lua/?.lua')
 table.insert(lua_runtime_path, 'lua/?/init.lua')
-local runtime_files = vim.api.nvim_get_runtime_file('lua', true)
+local runtime_files = vim.api.nvim_get_runtime_file('', true)
 -- Don't add ~/.config/nvim to the LSP libraries because that's just a symlink
 -- to ~/.dotfiles/nvim/lua, so when we're in ~/.dotfiles/nvim/lua we end up
 -- with duplicate symbols
