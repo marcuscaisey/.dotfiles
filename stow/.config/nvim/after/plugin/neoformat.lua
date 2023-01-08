9d4b89131e158b845dd026dafd2ecccbc76592bb
Marcus Caisey <marcus@teckna.com>
2023-01-08 15:57:02 +0000

    enable plz BUILD file formatting with neoformat

--- stow/.config/nvim/after/plugin/neoformat.lua
+++ stow/.config/nvim/after/plugin/neoformat.lua
@@ -1,21 +1,28 @@
 local Job = require('plenary.job')
 
-vim.g.neoformat_enabled_python = { 'black' }
-vim.g.neoformat_enabled_lua = { 'stylua' }
 vim.g.neoformat_enabled_go = { 'goimports' }
+vim.g.neoformat_enabled_lua = { 'stylua' }
+vim.g.neoformat_enabled_please = { 'plzformat' }
+vim.g.neoformat_enabled_python = { 'black' }
 vim.g.neoformat_enabled_sql = { 'sqlformat' }
-
-vim.g.neoformat_sql_sqlformat = {
-  exe = 'sqlformat',
-  args = { '--reindent', '--keywords', 'upper', '--identifiers', 'lower', '-' },
-  stdin = 1,
-}
 
 vim.g.neoformat_javascript_prettier = {
   exe = 'prettier',
   args = { '--stdin-filepath', '"%:p"', '--print-width', '100' },
   stdin = 1,
   try_node_exe = 1,
+}
+
+vim.g.neoformat_please_plzformat = {
+  exe = 'plz',
+  args = { 'format', '--write' },
+  replace = 1,
+}
+
+vim.g.neoformat_sql_sqlformat = {
+  exe = 'sqlformat',
+  args = { '--reindent', '--keywords', 'upper', '--identifiers', 'lower', '-' },
+  stdin = 1,
 }
 
 local wollemi = function()
