b83937fc962677a807ee2c693f18918d17d220ca
Marcus Caisey <marcus@teckna.com>
2023-01-13 18:35:06 +0000

    remove semantic token fix

--- stow/.config/nvim/after/plugin/lspconfig.lua
+++ stow/.config/nvim/after/plugin/lspconfig.lua
@@ -10,8 +10,6 @@ mason_lspconfig.setup({
 })
 
 local on_attach = function(client, bufnr)
-  client.server_capabilities.semanticTokensProvider = nil
-
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
   vim.keymap.set('n', '<c-s>', telescope_builtin.lsp_document_symbols, { buffer = bufnr })
   vim.keymap.set('n', '<leader>s', telescope_builtin.lsp_dynamic_workspace_symbols, { buffer = bufnr })
