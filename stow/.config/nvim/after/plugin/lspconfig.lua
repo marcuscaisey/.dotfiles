e39ddde1565107f7200cd39953e8dfe802c32fcc
Marcus Caisey <marcus@teckna.com>
2023-01-24 17:55:40 +0000

    make lsp mappings global

--- stow/.config/nvim/after/plugin/lspconfig.lua
+++ stow/.config/nvim/after/plugin/lspconfig.lua
@@ -9,44 +9,8 @@ mason_lspconfig.setup({
   automatic_installation = true,
 })
 
-local on_attach = function(client, bufnr)
-  if client then
-    -- disable lsp semantic highlighting, it's annoying and slower than treesitter
-    client.server_capabilities.semanticTokensProvider = nil
-  end
-
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
-  vim.keymap.set('n', '<c-s>', telescope_builtin.lsp_document_symbols, { buffer = bufnr })
-  vim.keymap.set('n', '<leader>s', telescope_builtin.lsp_dynamic_workspace_symbols, { buffer = bufnr })
-  vim.keymap.set('n', 'gd', telescope_builtin.lsp_definitions, { buffer = bufnr })
-  vim.keymap.set('n', 'gi', telescope_builtin.lsp_implementations, { buffer = bufnr })
-  vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, { buffer = bufnr })
-  vim.keymap.set('n', 'dK', vim.diagnostic.open_float, { buffer = bufnr })
-  vim.keymap.set('n', 'dr', vim.diagnostic.reset, { buffer = bufnr })
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr })
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = bufnr })
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = bufnr })
-  vim.keymap.set('n', '<leader>rc', vim.lsp.codelens.run, { buffer = bufnr })
-  vim.keymap.set('n', ']e', function()
-    vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity.WARN } })
-  end, { buffer = bufnr })
-  vim.keymap.set('n', '[e', function()
-    vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity.WARN } })
-  end, { buffer = bufnr })
-  vim.keymap.set('n', '<leader>dq', function()
-    local diagnostics = vim.diagnostic.get(0)
-    if #diagnostics == 0 then
-      print('No diagnostics')
-      vim.cmd.cclose()
-      return
-    end
-    local qf_items = vim.diagnostic.toqflist(diagnostics)
-    vim.fn.setqflist(qf_items)
-    vim.cmd.copen()
-    vim.cmd.cfirst()
-  end, { buffer = bufnr })
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
-  vim.keymap.set('v', '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
+local on_attach = function(client)
+  client.server_capabilities.semanticTokensProvider = nil
 end
 
 configs.please = {
@@ -85,14 +49,14 @@ lspconfig.gopls.setup({
       staticcheck = true,
     },
   },
-  on_attach = function(_, bufnr)
+  on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'BufWritePost', 'CursorHold' }, {
       callback = vim.lsp.codelens.refresh,
       group = vim.api.nvim_create_augroup('gopls', { clear = true }),
       buffer = bufnr,
       desc = 'Refresh codelenses when gopls is running',
     })
-    on_attach(nil, bufnr)
+    on_attach(client)
   end,
   root_dir = function(fname)
     local go_mod_root = util.root_pattern('go.mod')(fname)
@@ -213,3 +177,36 @@ vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticEr
 vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticWarn' })
 vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticInfo' })
 vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticHint' })
+
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+vim.keymap.set('n', '<c-s>', telescope_builtin.lsp_document_symbols)
+vim.keymap.set('n', '<leader>s', telescope_builtin.lsp_dynamic_workspace_symbols)
+vim.keymap.set('n', 'gd', telescope_builtin.lsp_definitions)
+vim.keymap.set('n', 'gi', telescope_builtin.lsp_implementations)
+vim.keymap.set('n', 'gr', telescope_builtin.lsp_references)
+vim.keymap.set('n', 'dK', vim.diagnostic.open_float)
+vim.keymap.set('n', 'dr', vim.diagnostic.reset)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>rc', vim.lsp.codelens.run)
+vim.keymap.set('n', ']e', function()
+  vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity.WARN } })
+end)
+vim.keymap.set('n', '[e', function()
+  vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity.WARN } })
+end)
+vim.keymap.set('n', '<leader>dq', function()
+  local diagnostics = vim.diagnostic.get(0)
+  if #diagnostics == 0 then
+    print('No diagnostics')
+    vim.cmd.cclose()
+    return
+  end
+  local qf_items = vim.diagnostic.toqflist(diagnostics)
+  vim.fn.setqflist(qf_items)
+  vim.cmd.copen()
+  vim.cmd.cfirst()
+end)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+vim.keymap.set('v', '<leader>ca', vim.lsp.buf.code_action)
