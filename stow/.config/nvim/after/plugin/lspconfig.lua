9521a0ba161a3852939ce36c8fae11ac50651305
Marcus Caisey <marcus@teckna.com>
2023-02-23 20:33:17 +0000

    Revert "enable lsp logs again"
    This reverts commit e881aab8ba2aca5b83c167c953818eb4941f8649.

--- stow/.config/nvim/after/plugin/lspconfig.lua
+++ stow/.config/nvim/after/plugin/lspconfig.lua
@@ -172,6 +172,8 @@ vim.diagnostic.config({
   severity_sort = true,
 })
 
+vim.lsp.set_log_level(vim.log.levels.OFF)
+
 vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticError' })
 vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticWarn' })
 vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticInfo' })
