db462ec6bc01a9b65cb548cb9ebd9085677f75c1
Marcus Caisey <marcus@teckna.com>
2023-04-15 18:51:43 +0100

    remove <leader>so mapping

--- stow/.config/nvim/lua/mappings.lua
+++ stow/.config/nvim/lua/mappings.lua
@@ -42,8 +42,6 @@ end)
 -- Don't overwrite unnamed register when pasting in visual mode
 vim.keymap.set('v', 'p', 'P')
 
-vim.keymap.set('n', '<leader>so', vim.cmd.source)
-
 -- Add to jumplist after relative line jumps
 vim.keymap.set('n', 'j', function()
   if vim.v.count > 0 then
