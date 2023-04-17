3dd385b59a7ad02c1916a9a22ab9133caa273e45
Marcus Caisey <marcus@teckna.com>
2023-04-17 14:18:33 +0100

    add n to formatoptions to format numbered lists

--- stow/.config/nvim/lua/options.lua
+++ stow/.config/nvim/lua/options.lua
@@ -15,6 +15,7 @@ vim.o.scrolloff = 10
 vim.o.shiftwidth = 4
 vim.o.shortmess = vim.o.shortmess .. 'S'
 vim.o.diffopt = vim.o.diffopt .. ',linematch:60'
+vim.opt.formatoptions:append('n')
 vim.o.showmode = false
 vim.o.signcolumn = 'yes:2'
 vim.o.smartcase = true
