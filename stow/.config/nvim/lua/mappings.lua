d777c696fd2c4322f28d2349b1e4e7a45c7526f9
Marcus Caisey <marcus@teckna.com>
2023-04-18 11:06:32 +0100

    print before sourcing in <leader>so

--- stow/.config/nvim/lua/mappings.lua
+++ stow/.config/nvim/lua/mappings.lua
@@ -40,8 +40,8 @@ vim.keymap.set('v', 'p', 'P')
 
 vim.keymap.set('n', '<leader>so', function()
   local file = vim.api.nvim_buf_get_name(0)
-  vim.cmd.source(file)
   print('Sourced ' .. file)
+  vim.cmd.source(file)
 end)
 
 -- Add to jumplist after relative line jumps
