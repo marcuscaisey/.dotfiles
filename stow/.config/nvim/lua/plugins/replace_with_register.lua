9df5354af898c691803f263b1b58e27c7f8ad2f0
Marcus Caisey <marcus@teckna.com>
2023-05-05 01:22:57 +0100

    override all replace with register mappings to disable grr mapping

--- stow/.config/nvim/lua/plugins/replace_with_register.lua
+++ stow/.config/nvim/lua/plugins/replace_with_register.lua
@@ -1,1 +1,3 @@
 vim.keymap.set('n', '<leader>r', '<Plug>ReplaceWithRegisterOperator')
+vim.keymap.set('n', '<leader>rr', '<Plug>ReplaceWithRegisterLine')
+vim.keymap.set('v', '<leader>r', '<Plug>ReplaceWithRegisterVisual')
