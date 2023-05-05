dbd1cf709eb292880b596cb4fb909c4306865517
Marcus Caisey <marcus@teckna.com>
2023-05-05 11:51:48 +0100

    add injection query for contract code in go

--- stow/.config/nvim/queries/go/injections.scm
+++ stow/.config/nvim/queries/go/injections.scm
@@ -7,3 +7,9 @@
 ((interpreted_string_literal) @sql (#lua-match? @sql "^\"%s*SELECT") (#offset! @sql 0 1 0 -1))
 ((interpreted_string_literal) @sql (#lua-match? @sql "^\"%s*INSERT") (#offset! @sql 0 1 0 -1))
 ((interpreted_string_literal) @sql (#lua-match? @sql "^\"%s*UPDATE") (#offset! @sql 0 1 0 -1))
+
+(
+  (raw_string_literal) @python
+  (#match? @python "api\\s*\\=\\s*(\"\\d\\.\\d\\.\\d\"|'\\d\\.\\d\\.\\d')\n")
+  (#offset! @python 0 1 0 -1)
+)
