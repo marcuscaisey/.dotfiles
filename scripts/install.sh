b55815c8fffad15fd0b4cd74c6b88679be687569
Marcus Caisey <marcus@teckna.com>
2023-07-22 16:26:20 +0100

    Revert "inline utils.sh into install.sh"
    This reverts commit 0959a2c776d13d9dab75823ecb1f372247cb55f1.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 
-# Return if we're running on osx
-osx() {
-  [ "$(uname)" = "Darwin" ]
-}
-
-# Return if we're running on osx
-linux() {
-  [ "$(uname)" = "Linux" ]
-}
-
-# echo wrapper which cycles between output colours on subsequent calls
-cecho() {
-  echo "$(tput setaf "${ci:-1}")$1$(tput sgr0)" && ci=$(( (${ci:-1} % 6) + 1 ))
-}
+source ~/.dotfiles/scripts/utils.sh
 
 if osx; then
   cecho "Installing homebrew..."
