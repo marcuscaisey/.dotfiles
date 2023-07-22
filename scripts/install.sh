291ce45f82243ba06f32dac06240093bf39419ed
Marcus Caisey <marcus@teckna.com>
2023-07-22 16:50:34 +0100

    add cmake to list of installed programs

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ if osx; then
   cecho "Installing homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  pkgs="zsh git tmux exa fzf fd bat ripgrep vifm ncurses stow git-delta tree-sitter curl make"
+  pkgs="zsh git tmux exa fzf fd bat ripgrep vifm ncurses stow git-delta tree-sitter curl make cmake"
   for pkg in $pkgs; do
       cecho "Installing $pkg..."
       brew install "$pkg"
@@ -22,7 +22,7 @@ if linux; then
   cecho "Updating list of available packages..."
   sudo apt update
 
-  pkgs="zsh git tmux cargo vifm stow curl make"
+  pkgs="zsh git tmux cargo vifm stow curl make cmake"
   for pkg in $pkgs; do
       cecho "Installing $pkg..."
       sudo apt install "$pkg"
