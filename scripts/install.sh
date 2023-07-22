0959a2c776d13d9dab75823ecb1f372247cb55f1
Marcus Caisey <marcus@teckna.com>
2023-07-22 15:47:50 +0100

    inline utils.sh into install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-source ~/.dotfiles/scripts/utils.sh
+# Return if we're running on osx
+osx() {
+  [ "$(uname)" = "Darwin" ]
+}
+
+# Return if we're running on osx
+linux() {
+  [ "$(uname)" = "Linux" ]
+}
+
+# echo wrapper which cycles between output colours on subsequent calls
+cecho() {
+  echo "$(tput setaf "${ci:-1}")$1$(tput sgr0)" && ci=$(( (${ci:-1} % 6) + 1 ))
+}
 
 if osx; then
   cecho "Installing homebrew..."
