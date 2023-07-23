68a6656bcdc45ffebfbdde7c6e31ae841df6eee4
Marcus Caisey <marcus@teckna.com>
2023-07-23 18:43:57 +0100

    add clone_or_pull helper to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,18 @@ set -euo pipefail
 
 source ~/.dotfiles/scripts/utils.sh
 
-# echo wrapper which cycles between output colours on subsequent calls
+# clone_or_pull <repository> <directory>
+# Clones a repository into a directory if it doesn't exist, otherwise pulls in the latest changes.
+clone_or_pull() {
+  if [ -d "$2" ]; then
+    git -C "$2" pull
+  else
+    git clone --depth 1 "$1" "$2"
+  fi
+}
+
+# cecho <message>
+# Displays a line of text in a different colour each time its called.
 cecho() {
   echo "$(tput setaf "${ci:-1}")$1$(tput sgr0)" && ci=$(( (${ci:-1} % 6) + 1 ))
 }
@@ -55,12 +66,12 @@ cecho "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
 cecho "Installing Oh My Zsh plugins: fast-syntax-highlighting, zsh-autosuggestions, zsh-vi-mode"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
-git clone https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
+clone_or_pull https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+clone_or_pull https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+clone_or_pull https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
 
 cecho "Installing tmux plugin manager"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+clone_or_pull https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 if osx; then
   cecho "Installing tmux-256color terminfo"
@@ -77,7 +88,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 cecho "Installing nvim"
 mkdir -p ~/scratch
-git clone https://github.com/neovim/neovim ~/scratch/neovim
+clone_or_pull https://github.com/neovim/neovim ~/scratch/neovim
 make -C ~/scratch/neovim CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/opt/neovim
 sudo make -C ~/scratch/neovim install
 
