99c7f9c002aacd0d2cc3556302ddaae2ad50e6a1
Marcus Caisey <marcus@teckna.com>
2023-08-28 18:29:30 +0100

    add mapping to yank has of fugitive commit patch

--- stow/.config/nvim/lua/plugins/fugitive.lua
+++ stow/.config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,17 @@
 vim.keymap.set('n', '<leader>gb', function()
   vim.cmd.Git('blame')
 end)
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = vim.api.nvim_create_augroup('fugitive_custom', { clear = true }),
+  pattern = { 'fugitive:///*' },
+  desc = 'Add mapping to Fugitive commit patch buffers to yank hash with <leader>y',
+  callback = function()
+    vim.keymap.set('n', '<leader>y', function()
+      local filepath = vim.api.nvim_buf_get_name(0)
+      local hash = filepath:match('.+/%.git//(.+)')
+      vim.fn.setreg('"', hash)
+      vim.fn.setreg('*', hash)
+      print(string.format('Yanked %s', hash))
+    end, { desc = 'Yank the hash of the commit patch', buffer = true })
+  end,
+})
