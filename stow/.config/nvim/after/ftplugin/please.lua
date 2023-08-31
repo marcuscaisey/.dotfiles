55666b61dfb2ad6a826d981bbf615d51c676fe8b
Marcus Caisey <marcus@teckna.com>
2023-08-31 11:46:18 +0100

    replace filetypes.lua with after/ftplugin

---
+++ stow/.config/nvim/after/ftplugin/please.lua
@@ -0,0 +1,1 @@
+vim.textwidth = 120
--- stow/.config/nvim/lua/options.lua
+++ stow/.config/nvim/lua/options.lua
@@ -1,5 +1,3 @@
-local filetypes = require('filetypes')
-
 vim.o.clipboard = 'unnamed'
 vim.o.cursorline = true
 vim.o.colorcolumn = '+1'
@@ -26,25 +24,6 @@ vim.o.tabstop = 4
 vim.o.termguicolors = true
 vim.o.updatetime = 100
 vim.opt.formatoptions:append('n')
-
-filetypes.setup({
-  go = { text_width = 120, indent_with_tabs = true },
-  html = { tab_size = 2 },
-  javascript = { tab_size = 2 },
-  json = { tab_size = 2 },
-  jsonc = { tab_size = 2 },
-  lua = { tab_size = 2, text_width = 120 },
-  markdown = { text_width = 100, auto_wrap = true },
-  please = { text_width = 120 },
-  proto = { text_width = 100 },
-  python = { text_width = 100 },
-  query = { tab_size = 2 },
-  scheme = { tab_size = 2 },
-  sh = { text_width = 120, tab_size = 2 },
-  sql = { tab_size = 2, text_width = 100 },
-  typescript = { tab_size = 2 },
-  zsh = { tab_size = 2 },
-})
 
 vim.filetype.add({
   extension = {
