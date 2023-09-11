f1868a83a9676a47ff26433126a0110e3d0ba1da
Marcus Caisey <marcus@teckna.com>
2023-09-12 00:02:04 +0200

    sort luansip imports

--- stow/.config/nvim/lua/plugins/luasnip.lua
+++ stow/.config/nvim/lua/plugins/luasnip.lua
@@ -1,15 +1,14 @@
 local ls = require('luasnip')
 local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
-local t = ls.text_node
 local d = ls.dynamic_node
-local sn = ls.snippet_node
-local ps = ls.parser.parse_snippet
-local fmt = require('luasnip.extras.fmt').fmt
-local rep = require('luasnip.extras').rep
-local nonempty = require('luasnip.extras').nonempty
-local types = require('luasnip.util.types')
+local extras = require("luasnip.extras")
+local rep = extras.rep
+local fmt = require("luasnip.extras.fmt").fmt
+local types = require("luasnip.util.types")
 
 ls.config.setup({
   updateevents = 'TextChanged,TextChangedI',
