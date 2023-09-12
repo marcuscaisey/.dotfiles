f4182daef7420d0e62b223bb78b7e565b4ffd25c
Marcus Caisey <marcus@teckna.com>
2023-09-12 21:21:25 +0100

    move close qf autocmd to qf ftplugin

--- stow/.config/nvim/after/ftplugin/qf.lua
+++ stow/.config/nvim/after/ftplugin/qf.lua
@@ -1,1 +1,23 @@
 vim.opt_local.wrap = false
+
+if vim.g.loaded_qf then
+  return
+end
+vim.g.loaded_qf = true
+
+vim.api.nvim_create_autocmd('WinClosed', {
+  callback = function(args)
+    local closed_winid = tonumber(args.match)
+    for _, winid in ipairs(vim.api.nvim_list_wins()) do
+      if winid ~= closed_winid then
+        local bufnr = vim.api.nvim_win_get_buf(winid)
+        if vim.bo[bufnr].filetype ~= 'qf' then
+          return
+        end
+      end
+    end
+    vim.cmd.quitall()
+  end,
+  group = vim.api.nvim_create_augroup('qf', { clear = true }),
+  desc = 'Quit if last open window is the quickfix',
+})
