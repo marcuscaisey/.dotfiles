289db25f96bde63360e53ff974fb006350f47aa8
Marcus Caisey <marcus@teckna.com>
2023-09-12 23:12:10 +0100

    move wollemi autocmd go to ftplugin

--- stow/.config/nvim/after/ftplugin/go.lua
+++ stow/.config/nvim/after/ftplugin/go.lua
@@ -1,2 +1,36 @@
 vim.opt_local.textwidth = 120
 vim.opt_local.expandtab = false
+
+if vim.g.loaded_go then
+  return
+end
+vim.g.loaded_go = true
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  group = vim.api.nvim_create_augroup('go', { clear = true }),
+  pattern = { '*.go' },
+  desc = 'Run wollemi on parent directory',
+  callback = function(args)
+    if #vim.fs.find('.plzconfig', { upward = true, path = args.file }) < 1 then
+      return
+    end
+    local output_lines = {}
+    local function on_output(_, line)
+      table.insert(output_lines, line)
+    end
+    local function on_exit()
+      vim.print(table.concat(output_lines, '\n'))
+    end
+    vim.system({ 'wollemi', 'gofmt' }, {
+      -- Run in the directory of the saved file since wollemi won't run outside of a plz repo
+      cwd = vim.fs.dirname(args.file),
+      env = {
+        -- wollemi needs GOROOT to be set
+        GOROOT = vim.trim(vim.system({ 'go', 'env', 'GOROOT' }):wait().stdout),
+        PATH = os.getenv('PATH') or '',
+      },
+      stdout = on_output,
+      stderr = on_output,
+    }, on_exit)
+  end,
+})
--- stow/.config/nvim/lua/misc.lua
+++ stow/.config/nvim/lua/misc.lua
@@ -72,37 +72,6 @@ vim.api.nvim_create_autocmd({ 'DirChanged' }, {
   desc = 'Rename the tmux window to $cwd:nvim',
 })
 
-vim.api.nvim_create_autocmd('BufWritePost', {
-  callback = function()
-    local filepath = vim.api.nvim_buf_get_name(0)
-    local plz_root = vim.fs.find('.plzconfig', { upward = true, path = filepath })[1]
-    if not plz_root then
-      return
-    end
-    local output_lines = {}
-    local on_output = function(_, line)
-      table.insert(output_lines, line)
-    end
-    local on_exit = function()
-      vim.print(table.concat(output_lines, '\n'))
-    end
-    vim.system({ 'wollemi', 'gofmt' }, {
-      -- Run in the directory of the saved file since wollemi won't run outside of a plz repo
-      cwd = vim.fs.dirname(filepath),
-      env = {
-        -- wollemi needs GOROOT to be set
-        GOROOT = vim.trim(vim.system({ 'go', 'env', 'GOROOT' }):wait().stdout),
-        PATH = vim.fn.getenv('PATH'),
-      },
-      stdout = on_output,
-      stderr = on_output,
-    }, on_exit)
-  end,
-  pattern = { '*.go' },
-  group = group,
-  desc = 'Run wollemi on parent directory of go file',
-})
-
 local diff_augroup = vim.api.nvim_create_augroup('diff', { clear = true })
 vim.api.nvim_create_autocmd('VimEnter', {
   group = diff_augroup,
