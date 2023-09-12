1de23c8fbfc24209137d6d1e6fe7bb7240a294f1
Marcus Caisey <marcus@teckna.com>
2023-09-13 00:26:36 +0100

    fix augroup creation in tmux.lua

--- stow/.config/nvim/lua/tmux.lua
+++ stow/.config/nvim/lua/tmux.lua
@@ -1,4 +1,4 @@
-local augroup = vim.nvim_create_augroup('tmux', { clear = true })
+local augroup = vim.api.nvim_create_augroup('tmux', { clear = true })
 
 vim.api.nvim_create_autocmd({ 'VimLeave' }, {
   callback = function()
