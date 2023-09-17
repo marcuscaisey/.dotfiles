0b345ee262468c6ac7be3f8e803ac1ce43b52a1b
Marcus Caisey <marcus@teckna.com>
2023-09-17 23:54:35 +0100

    remove nvim-dap-virtual-text

--- stow/.config/nvim/lua/plugins.lua
+++ stow/.config/nvim/lua/plugins.lua
@@ -55,12 +55,6 @@ packer.startup({
       end,
     })
     use({
-      'theHamsta/nvim-dap-virtual-text',
-      config = function()
-        require('nvim-dap-virtual-text').setup({})
-      end,
-    })
-    use({
       'rcarriga/nvim-dap-ui',
       config = function()
         require('plugins.dap_ui')
