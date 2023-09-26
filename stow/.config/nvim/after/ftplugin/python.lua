e12f10043e648fdfb67aad7aaa64e2e00bb4cf13
Marcus Caisey <marcus@teckna.com>
2023-09-26 09:24:09 +0100

    overwrite formatexpr for python

--- stow/.config/nvim/after/ftplugin/python.lua
+++ stow/.config/nvim/after/ftplugin/python.lua
@@ -1,1 +1,4 @@
 vim.opt_local.textwidth = 100
+-- If this is not set, it gets automatically overridden to v:lua.vim.lsp.formatexpr() by Neovim when the Python language
+-- server attaches which breaks formatting with gq.
+vim.opt_local.formatexpr = ' '
