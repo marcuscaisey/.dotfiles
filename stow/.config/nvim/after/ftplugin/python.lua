1005abed4ea8d11d4e7d94f6ad0c78c74eb9748b
Marcus Caisey <marcus@teckna.com>
2023-10-01 13:21:25 +0100

    don't autowrap python files

--- stow/.config/nvim/after/ftplugin/python.lua
+++ stow/.config/nvim/after/ftplugin/python.lua
@@ -2,3 +2,5 @@ vim.opt_local.textwidth = 100
 -- If this is not set, it gets automatically overridden to v:lua.vim.lsp.formatexpr() by Neovim when the Python language
 -- server attaches which breaks formatting with gq.
 vim.opt_local.formatexpr = ' '
+-- Don't auto-wrap using textwidth.
+vim.opt_local.formatoptions:remove('t')
