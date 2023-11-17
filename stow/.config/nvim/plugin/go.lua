90bf49db955e003dd2eac28d0d8b54fcafff0cb8
Marcus Caisey <marcus@teckna.com>
2023-11-17 16:28:10 +0000

    get rid of backwards compatible puku snippet

--- stow/.config/nvim/plugin/go.lua
+++ stow/.config/nvim/plugin/go.lua
@@ -22,27 +22,3 @@ vim.api.nvim_create_autocmd('BufWritePost', {
     })
   end,
 })
-
--- vim.api.nvim_create_autocmd('BufWritePost', {
---   group = vim.api.nvim_create_augroup('go', { clear = true }),
---   pattern = { '*.go' },
---   desc = 'Run puku on parent directory',
---   callback = function(args)
---     if #vim.fs.find('.plzconfig', { upward = true, path = args.file }) < 1 then
---       return
---     end
---     local function output_handler(level)
---       return vim.schedule_wrap(function(_, data)
---         local output = table.concat(data, '')
---         if #output > 0 then
---           vim.notify(output:gsub('\n$', ''), level)
---         end
---       end)
---     end
---     vim.fn.jobstart({ 'puku', 'fmt', '...' }, {
---       cwd = vim.fs.dirname(args.file),
---       on_stdout = output_handler(vim.log.levels.INFO),
---       on_stderr = output_handler(vim.log.levels.ERROR),
---     })
---   end,
--- })
