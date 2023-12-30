7d03802c58bc45037d36094b7a55704aebd69941
Marcus Caisey <marcus@teckna.com>
2023-12-30 14:39:58 +0000

    remove qf auto close autocmd

--- stow/.config/nvim/plugin/qf.lua
+++ stow/.config/nvim/plugin/qf.lua
@@ -1,20 +1,3 @@
-vim.api.nvim_create_autocmd('WinClosed', {
-  callback = function(args)
-    local closed_winid = tonumber(args.match)
-    for _, winid in ipairs(vim.api.nvim_list_wins()) do
-      if winid ~= closed_winid then
-        local bufnr = vim.api.nvim_win_get_buf(winid)
-        if vim.bo[bufnr].filetype ~= 'qf' then
-          return
-        end
-      end
-    end
-    vim.cmd.quitall()
-  end,
-  group = vim.api.nvim_create_augroup('qf', { clear = true }),
-  desc = 'Quit if last open window is the quickfix',
-})
-
 vim.keymap.set('n', '<leader>q', function()
   local qf_window_id = vim.fn.getqflist({ winid = 0 }).winid
   local qf_open = qf_window_id > 0
