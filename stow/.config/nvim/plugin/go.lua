2d56565e6eb9d0ac0a2680eb3e3dd895e550ed8c
Marcus Caisey <marcus@teckna.com>
2024-01-12 20:50:13 +0000

    add go organizeImports autocmd

--- stow/.config/nvim/plugin/go.lua
+++ stow/.config/nvim/plugin/go.lua
@@ -22,3 +22,26 @@ vim.api.nvim_create_autocmd('BufWritePost', {
     })
   end,
 })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = vim.api.nvim_create_augroup('go', { clear = true }),
+  pattern = '*.go',
+  desc = 'Organize imports on save',
+  callback = function(args)
+    local params = vim.lsp.util.make_range_params() ---@type lsp.CodeActionParams
+    params.context = {
+      diagnostics = {},
+      only = { 'source.organizeImports' },
+    }
+    local result = vim.lsp.buf_request_sync(args.buf, protocol.Methods.textDocument_codeAction, params)
+    for client_id, err_or_result in pairs(result or {}) do
+      for _, code_action in pairs(err_or_result.result or {}) do
+        ---@cast code_action lsp.CodeAction
+        if code_action.edit then
+          local offset_encoding = vim.lsp.get_client_by_id(client_id).offset_encoding or 'utf-16'
+          vim.lsp.util.apply_workspace_edit(code_action.edit, offset_encoding)
+        end
+      end
+    end
+  end,
+})
