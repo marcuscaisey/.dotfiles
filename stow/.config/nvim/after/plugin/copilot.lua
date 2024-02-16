3bc563af71006dc7317da847f0a8cdaf86f47b6b
Marcus Caisey <marcus@teckna.com>
2024-02-16 17:14:08 +0000

    enable copilot in markdown files

--- stow/.config/nvim/after/plugin/copilot.lua
+++ stow/.config/nvim/after/plugin/copilot.lua
@@ -6,8 +6,12 @@ if os.getenv('NVIM_DISABLE_COPILOT') then
   vim.cmd.Copilot('disable')
 end
 
+vim.g.copilot_filetypes = {
+  markdown = true,
+}
+
 local enabled = true
-vim.keymap.set('n', '<leader>cc', function ()
+vim.keymap.set('n', '<leader>cc', function()
   if enabled then
     vim.cmd.Copilot('disable')
     print('Disabled copilot')
