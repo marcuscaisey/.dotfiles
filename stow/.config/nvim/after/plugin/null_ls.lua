142fce7c0c2a49ce05367b7a8173c8ec5095509b
Marcus Caisey <marcus@teckna.com>
2024-02-21 22:58:31 +0000

    stop using mypy

--- stow/.config/nvim/after/plugin/null_ls.lua
+++ stow/.config/nvim/after/plugin/null_ls.lua
@@ -5,7 +5,6 @@ end
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.prettier,
