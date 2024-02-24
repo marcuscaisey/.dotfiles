bd458d9f84207b6d1cc166e5f7c8899700bd7107
Marcus Caisey <marcus@teckna.com>
2024-02-24 12:24:09 +0000

    only refresh codelens in buffers that support it

--- stow/.config/nvim/plugin/lsp.lua
+++ stow/.config/nvim/plugin/lsp.lua
@@ -16,7 +16,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if client and client.supports_method(protocol.Methods.textDocument_codeLens) then
       vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'BufWritePost', 'CursorHold' }, {
-        callback = vim.lsp.codelens.refresh,
+        callback = function()
+          vim.lsp.codelens.refresh({ bufnr = args.buf })
+        end,
         group = augroup,
         buffer = args.buf,
         desc = 'Refresh codelenses automatically in this buffer',
