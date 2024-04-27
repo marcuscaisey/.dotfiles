39fee38910531a817796e98f55516593759474cd
Marcus Caisey <marcus@teckna.com>
2024-04-27 18:27:05 +0100

    remove mappings for lsp rename and code action

--- stow/.config/nvim/plugin/lsp.lua
+++ stow/.config/nvim/plugin/lsp.lua
@@ -6,9 +6,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = augroup,
   desc = 'Set LSP keymaps and create codelens autocmd',
   callback = function(args)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = true })
     vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, { buffer = true })
-    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = true })
     vim.keymap.set('n', '<leader>fm', function()
       vim.lsp.buf.format({ timeout_ms = 5000 })
     end, { buffer = true })
