f8ec3403394db39e514802c08ea9c75c431e0f74
Marcus Caisey <marcus@teckna.com>
2024-04-27 18:06:41 +0100

    replace luasnip with native snippets

--- stow/.config/nvim/after/plugin/cmp.lua
+++ stow/.config/nvim/after/plugin/cmp.lua
@@ -2,27 +2,16 @@ local ok, cmp = pcall(require, 'cmp')
 if not ok then
   return
 end
-local ok, luasnip = pcall(require, 'luasnip')
-if not ok then
-  return
-end
 
 local source_name_to_menu = {
   nvim_lsp = '[LSP]',
   buffer = '[BUFFER]',
-  luasnip = '[SNIP]',
 }
 
 cmp.setup({
   sources = {
     { name = 'nvim_lsp' },
     { name = 'buffer' },
-    { name = 'luasnip' },
-  },
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
   },
   mapping = cmp.mapping.preset.insert({
     ['<c-space>'] = cmp.mapping.complete(),
