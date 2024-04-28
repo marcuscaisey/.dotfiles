4044ee759ed400a24191d2eac6d93afd161dd60c
Marcus Caisey <marcus@teckna.com>
2024-04-29 00:41:20 +0100

    add nvim-dap-ui dep nvim-io

--- stow/.config/nvim/after/plugin/packer.lua
+++ stow/.config/nvim/after/plugin/packer.lua
@@ -23,7 +23,7 @@ packer.startup({
     use({ 'hrsh7th/cmp-nvim-lsp', requires = { 'hrsh7th/nvim-cmp' } })
     use({ 'hrsh7th/cmp-buffer', requires = { 'hrsh7th/nvim-cmp' } })
     use({ 'mfussenegger/nvim-dap' })
-    use({ 'rcarriga/nvim-dap-ui', requires = { 'mfussenegger/nvim-dap' } })
+    use({ 'rcarriga/nvim-dap-ui', requires = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' } })
     use({ 'ray-x/lsp_signature.nvim' })
     use({ 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons' } })
     use({ 'lewis6991/gitsigns.nvim' })
