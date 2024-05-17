11f851a072b89df71cb8987149eeab338bfd41af
Marcus Caisey <marcus@teckna.com>
2024-05-17 01:17:04 +0100

    remove deprecated diagnostic usage

--- stow/.config/nvim/plugin/diagnostic.lua
+++ stow/.config/nvim/plugin/diagnostic.lua
@@ -1,6 +1,6 @@
 local virtual_text_float_config = {
   source = true,
-  ---@param diagnostic Diagnostic
+  ---@param diagnostic vim.Diagnostic
   ---@return string
   format = function(diagnostic)
     local source_prefix = string.format('%s: ', diagnostic.source)
@@ -13,12 +13,15 @@ vim.diagnostic.config({
   virtual_text = virtual_text_float_config,
   source = true,
   severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '󰌵',
+    },
+  },
 })
-
-vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticError' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticWarn' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticInfo' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticHint' })
 
 vim.keymap.set('n', ']d', function()
   vim.diagnostic.goto_next({ wrap = false })
@@ -27,11 +30,11 @@ vim.keymap.set('n', '[d', function()
   vim.diagnostic.goto_prev({ wrap = false })
 end)
 vim.keymap.set('n', '<leader>dd', function()
-  if vim.diagnostic.is_disabled() then
+  if not vim.diagnostic.is_enabled() then
     vim.diagnostic.enable()
     print('Enabled diagnostics')
   else
-    vim.diagnostic.disable()
+    vim.diagnostic.enable(false)
     print('Disabled diagnostics')
   end
 end)
