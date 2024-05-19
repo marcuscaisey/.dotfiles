a9320d54f18f943e8fd896537c6548cf4b2a67d8
Marcus Caisey <marcus@teckna.com>
2024-05-19 23:06:34 +0100

    set GOROOT env var in gopls root_dir function

--- stow/.config/nvim/after/plugin/lspconfig.lua
+++ stow/.config/nvim/after/plugin/lspconfig.lua
@@ -51,6 +51,62 @@ lspconfig.cmake.setup({})
 
 lspconfig.dartls.setup({})
 
+---@param plz_root string
+---@return string? goroot
+---@return string? errmsg
+local function plz_goroot(plz_root)
+  local gotool_res = vim.system({ 'plz', '--repo_root', plz_root, 'query', 'config', 'plugin.go.gotool' }):wait()
+  local gotool = 'go'
+  if gotool_res.code == 0 then
+    gotool = vim.trim(gotool_res.stdout)
+  elseif not gotool_res.stderr:match('Settable field not defined') then
+    return nil, string.format('querying value of plugin.go.gotool: %s', gotool_res.stderr)
+  end
+
+  if vim.startswith(gotool, ':') or vim.startswith(gotool, '//') then
+    gotool = gotool:gsub('|go$', '')
+    local gotool_output_res = vim.system({ 'plz', '--repo_root', plz_root, 'query', 'output', gotool }):wait()
+    if gotool_output_res.code > 0 then
+      return nil,
+        string.format('querying output of plugin.go.gotool target %s: %s', gotool, gotool_output_res.stderr)
+    end
+    return vim.fs.joinpath(plz_root, vim.trim(gotool_output_res.stdout))
+  end
+
+  if vim.startswith(gotool, '/') then
+    if not vim.uv.fs_stat(gotool) then
+      return nil, string.format('plugin.go.gotool %s does not exist', gotool)
+    end
+    local goroot_res = vim.system({ gotool, 'env', 'GOROOT' }):wait()
+    if goroot_res.code == 0 then
+      return vim.trim(goroot_res.stdout)
+    else
+      return nil, string.format('%s env GOROOT: %s', gotool, goroot_res.stderr)
+    end
+  end
+
+  local build_paths_res = vim.system({ 'plz', '--repo_root', plz_root, 'query', 'config', 'build.path' }):wait()
+  if build_paths_res.code > 0 then
+    return nil, string.format('querying value of build.path: %s', build_paths_res.stderr)
+  end
+  local build_paths = vim.trim(build_paths_res.stdout)
+  for build_path in vim.gsplit(build_paths, '\n') do
+    for build_path_part in vim.gsplit(build_path, ':') do
+      local go = vim.fs.joinpath(build_path_part, gotool)
+      if vim.uv.fs_stat(go) then
+        local goroot_res = vim.system({ go, 'env', 'GOROOT' }):wait()
+        if goroot_res.code == 0 then
+          return vim.trim(goroot_res.stdout)
+        else
+          return nil, string.format('%s env GOROOT: %s', go, goroot_res.stderr)
+        end
+      end
+    end
+  end
+
+  return nil, string.format('plugin.go.gotool %s not found in build.path %s', gotool, build_paths:gsub('\n', ':'))
+end
+
 lspconfig.gopls.setup({
   settings = {
     gopls = {
@@ -65,10 +121,20 @@ lspconfig.gopls.setup({
     },
   },
   root_dir = function(fname)
-    local plzconfig_dir = util.root_pattern('.plzconfig')(fname)
-    if plzconfig_dir and vim.fs.basename(plzconfig_dir) == 'src' then
-      vim.env.GOPATH = string.format('%s:%s/plz-out/go', vim.fs.dirname(plzconfig_dir), plzconfig_dir)
+    local plz_root = util.root_pattern('.plzconfig')(fname)
+    if plz_root and vim.fs.basename(plz_root) == 'src' then
+      vim.env.GOPATH = string.format('%s:%s/plz-out/go', vim.fs.dirname(plz_root), plz_root)
       vim.env.GO111MODULE = 'off'
+      local goroot, err = plz_goroot(plz_root)
+      if goroot then
+        if not vim.uv.fs_stat(goroot) then
+          vim.notify(string.format('GOROOT for plz repo %s does not exist: %s', plz_root, goroot), vim.log.levels.WARN)
+        else
+          vim.env.GOROOT = goroot
+        end
+      else
+        vim.notify(string.format('Determining GOROOT for plz repo %s: %s', plz_root, err), vim.log.levels.WARN)
+      end
       return vim.fn.getcwd()
     end
 
@@ -76,8 +142,6 @@ lspconfig.gopls.setup({
     if gowork_or_gomod_dir then
       return gowork_or_gomod_dir
     end
-
-    return vim.fn.getcwd()
   end,
 })
 
