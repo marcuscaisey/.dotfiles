49de81d8ae1b00ffcf38bf07385642eff7231000
Marcus Caisey <marcus@teckna.com>
2024-05-22 17:46:39 +0100

    set lsp keymaps outside LspAttach autocmd

--- stow/.config/nvim/plugin/lsp.lua
+++ stow/.config/nvim/plugin/lsp.lua
@@ -4,13 +4,8 @@ local augroup = vim.api.nvim_create_augroup('lsp', { clear = true })
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = augroup,
-  desc = 'Set LSP keymaps and create codelens autocmd',
+  desc = 'Create codelens autocmd',
   callback = function(args)
-    vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, { buffer = true })
-    vim.keymap.set('n', '<leader>fm', function()
-      vim.lsp.buf.format({ timeout_ms = 5000 })
-    end, { buffer = true })
-
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if client and client.supports_method(protocol.Methods.textDocument_codeLens) then
       vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'BufWritePost', 'CursorHold' }, {
@@ -30,14 +25,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 vim.lsp.set_log_level(vim.log.levels.OFF)
 
-vim.keymap.set('n', 'crn', function()
-  vim.lsp.buf.rename()
-end, { desc = 'vim.lsp.buf.rename()' })
+vim.keymap.set('n', 'crl', vim.lsp.codelens.run, { desc = 'vim.lsp.codelens.run()' })
+vim.keymap.set('n', 'crf', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format()' })
+vim.keymap.set('n', 'crn', vim.lsp.buf.rename, { desc = 'vim.lsp.buf.rename()' })
 
 local function map_codeaction(mode, lhs)
-  vim.keymap.set(mode, lhs, function()
-    vim.lsp.buf.code_action()
-  end, { desc = 'vim.lsp.buf.code_action()' })
+  vim.keymap.set(mode, lhs, vim.lsp.buf.code_action, { desc = 'vim.lsp.buf.code_action()' })
 end
 map_codeaction('n', 'crr')
 map_codeaction('x', '<C-R>r')
