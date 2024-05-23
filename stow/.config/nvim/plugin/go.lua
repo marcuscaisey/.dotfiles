f79971295028df0e2488ce3dce257bf8939e0623
Marcus Caisey <marcus@teckna.com>
2024-05-23 17:59:02 +0100

    use args.match instead of args.file in puku autocmd

--- stow/.config/nvim/plugin/go.lua
+++ stow/.config/nvim/plugin/go.lua
@@ -7,10 +7,10 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = { '*.go' },
   desc = 'Run puku on saved file',
   callback = function(args)
-    if not vim.fs.root(args.file, '.plzconfig') then
+    if not vim.fs.root(args.match, '.plzconfig') then
       return
     end
-    vim.system({ 'puku', 'fmt', args.file }, nil, function(res)
+    vim.system({ 'puku', 'fmt', args.match }, nil, function(res)
       local output = res.code == 0 and vim.trim(res.stdout) or vim.trim(res.stderr)
       if output ~= '' then
         vim.schedule(function()
