6bbda7807213af173a43ca0803ecf0158500555a
Marcus Caisey <marcus@teckna.com>
2024-05-31 00:25:36 +0100

    use default lsp mappings again

--- stow/.config/nvim/plugin/lsp.lua
+++ stow/.config/nvim/plugin/lsp.lua
@@ -23,13 +23,5 @@ vim.api.nvim_create_autocmd('LspAttach', {
 vim.lsp.log.set_format_func(vim.inspect)
 vim.lsp.set_log_level(vim.log.levels.OFF)
 
-vim.keymap.set('n', 'crl', vim.lsp.codelens.run, { desc = 'vim.lsp.codelens.run()' })
-vim.keymap.set('n', 'crf', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format()' })
-vim.keymap.set('n', 'crn', vim.lsp.buf.rename, { desc = 'vim.lsp.buf.rename()' })
-
-local function map_codeaction(mode, lhs)
-  vim.keymap.set(mode, lhs, vim.lsp.buf.code_action, { desc = 'vim.lsp.buf.code_action()' })
-end
-map_codeaction('n', 'crr')
-map_codeaction('x', '<C-R>r')
-map_codeaction('x', '<C-R><C-R>')
+vim.keymap.set('n', 'grl', vim.lsp.codelens.run, { desc = 'vim.lsp.codelens.run()' })
+vim.keymap.set('n', 'grf', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format()' })
