906b5fa0eb2560450b9ff53b4b650c8ba065ba80
Marcus Caisey <marcus@teckna.com>
2024-07-07 22:08:43 +0100

    replace prettier with prettierd

--- stow/.config/nvim/after/plugin/null_ls.lua
+++ stow/.config/nvim/after/plugin/null_ls.lua
@@ -7,6 +7,6 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd,
   },
 })
