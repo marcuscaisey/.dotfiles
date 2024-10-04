503288bf1c7e5de4ba39d106ed2b0cf23edc535a
Marcus Caisey <marcus@teckna.com>
2024-10-04 09:45:06 +0100

    add back snippet mappings

---
+++ stow/.config/nvim/plugin/snippet.lua
@@ -0,0 +1,15 @@
+vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+  if vim.snippet.active({ direction = 1 }) then
+    return '<cmd>lua vim.snippet.jump(1)<cr>'
+  else
+    return '<Tab>'
+  end
+end, { desc = 'vim.snippet.jump(1)', expr = true })
+
+vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+  if vim.snippet.active({ direction = 1 }) then
+    return '<cmd>lua vim.snippet.jump(-1)<cr>'
+  else
+    return '<Tab>'
+  end
+end, { desc = 'vim.snippet.jump(-1)', expr = true })
