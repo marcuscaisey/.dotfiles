72518954b75c536f524454901319d7fc9d4be66b
Marcus Caisey <marcus@teckna.com>
2024-11-02 17:15:05 +0000

    remove loxfmt from null ls config

--- stow/.config/nvim/after/plugin/null_ls.lua
+++ stow/.config/nvim/after/plugin/null_ls.lua
@@ -2,22 +2,11 @@ local ok, null_ls = pcall(require, 'null-ls')
 if not ok then
   return
 end
-local helpers = require('null-ls.helpers')
-
-local loxfmt = {
-  method = null_ls.methods.FORMATTING,
-  filetypes = { 'lox' },
-  generator = helpers.formatter_factory({
-    command = 'loxfmt',
-    to_stdin = true,
-  }),
-}
 
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.prettierd,
-    loxfmt,
   },
 })
