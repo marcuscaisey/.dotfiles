af0155db7bc03b9e2e84f8d8ce636deb82c37b8c
Marcus Caisey <marcus@teckna.com>
2024-11-30 00:58:14 +0000

    build loxls everytime

--- stow/.config/nvim/after/ftplugin/lox.lua
+++ stow/.config/nvim/after/ftplugin/lox.lua
@@ -3,6 +3,6 @@ vim.opt_local.textwidth = 120
 
 vim.lsp.start({
   name = 'loxls',
-  cmd = { 'loxls' },
+  cmd = { 'go', 'run', 'github.com/marcuscaisey/lox/loxls' },
   root_dir = vim.fs.root(0, '.git'),
 })
