eec8f97b6e0ed45e497f6f684bccbfee2ce77ee5
Marcus Caisey <marcus@teckna.com>
2024-12-08 11:45:28 +0000

    remove vim.notify override

--- stow/.config/nvim/init.lua
+++ stow/.config/nvim/init.lua
@@ -6,16 +6,3 @@ vim.g.loaded_matchit = 1
 
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.deprecate = function() end
-
-local original_notify = vim.notify
-
----@param msg string
----@param level integer?
----@param opts table?
----@diagnostic disable-next-line: duplicate-set-field
-function vim.notify(msg, level, opts)
-  if vim.startswith(msg, 'position_encoding param is required in vim.lsp.util.make_range_params.') then
-    return
-  end
-  return original_notify(msg, level, opts)
-end
