2e80733fb35f1c955d1c76c5b22cf33d34adf0ac
Marcus Caisey <marcus@teckna.com>
2025-03-29 16:53:24 +0800

    remove kubectl from install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -37,7 +37,7 @@ if osx; then
   cecho "Updating list of available packages"
   brew update
 
-  pkgs="zsh git tmux eza fzf fd bat ripgrep vifm stow git-delta curl make cmake gettext gcc kubectl"
+  pkgs="zsh git tmux eza fzf fd bat ripgrep vifm stow git-delta curl make cmake gettext gcc"
   cecho "Installing $pkgs"
   brew install $pkgs
 fi
