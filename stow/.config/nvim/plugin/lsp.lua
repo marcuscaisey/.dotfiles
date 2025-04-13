662173ba0db203fc717bc0db5c0e600ff7b7978e
Marcus Caisey <marcus@teckna.com>
2025-04-14 00:21:45 +0800

    reorder lsp.lua

--- stow/.config/nvim/plugin/lsp.lua
+++ stow/.config/nvim/plugin/lsp.lua
@@ -1,36 +1,5 @@
 local augroup = vim.api.nvim_create_augroup('lsp', { clear = true })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = augroup,
-  desc = 'Create codelens autocmd and format mapping',
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-    if client:supports_method(vim.lsp.protocol.Methods.textDocument_codeLens) then
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'BufWritePost', 'CursorHold' }, {
-        callback = function()
-          vim.lsp.codelens.refresh({ bufnr = args.buf })
-        end,
-        group = augroup,
-        buffer = args.buf,
-        desc = 'Refresh codelenses',
-      })
-    end
-
-    if client:supports_method(vim.lsp.protocol.Methods.textDocument_formatting) and vim.bo.formatprg == '' then
-      vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format()', buffer = args.buf })
-    end
-  end,
-})
-
-vim.lsp.log.set_format_func(vim.inspect)
-if vim.env.NVIM_DISABLE_LSP_LOGGING == 'true' then
-  vim.lsp.set_log_level(vim.log.levels.OFF)
-end
-if vim.env.NVIM_LSP_LOG_LEVEL then
-  vim.lsp.set_log_level(vim.env.NVIM_LSP_LOG_LEVEL)
-end
-
 local enabled_lsps = {
   'bashls',
   'clangd',
@@ -46,15 +15,6 @@ if vim.env.NVIM_DISABLE_GOLANGCI_LINT ~= 'true' then
   table.insert(enabled_lsps, 'golangci_lint_ls')
 end
 vim.lsp.enable(enabled_lsps)
-
-if vim.env.NVIM_ENABLE_LSP_DEVTOOLS == 'true' then
-  for _, name in ipairs(enabled_lsps) do
-    vim.lsp.config(name, {
-      cmd = { 'lsp-devtools', 'agent', '--', unpack(vim.lsp.config[name].cmd) },
-    })
-    vim.print(vim.lsp.config[name].cmd)
-  end
-end
 
 vim.lsp.config('clangd', {
   filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' }, -- Default excluding proto
@@ -88,5 +48,44 @@ vim.lsp.config('ts_ls', {
     client.server_capabilities.documentFormattingProvider = false
   end,
 })
+
+if vim.env.NVIM_ENABLE_LSP_DEVTOOLS == 'true' then
+  for _, name in ipairs(enabled_lsps) do
+    vim.lsp.config(name, {
+      cmd = { 'lsp-devtools', 'agent', '--', unpack(vim.lsp.config[name].cmd) },
+    })
+  end
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = augroup,
+  desc = 'Create codelens autocmd and format mapping',
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+    if client:supports_method(vim.lsp.protocol.Methods.textDocument_codeLens) then
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'BufWritePost', 'CursorHold' }, {
+        callback = function()
+          vim.lsp.codelens.refresh({ bufnr = args.buf })
+        end,
+        group = augroup,
+        buffer = args.buf,
+        desc = 'Refresh codelenses',
+      })
+    end
+
+    if client:supports_method(vim.lsp.protocol.Methods.textDocument_formatting) and vim.bo.formatprg == '' then
+      vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format()', buffer = args.buf })
+    end
+  end,
+})
+
+vim.lsp.log.set_format_func(vim.inspect)
+if vim.env.NVIM_DISABLE_LSP_LOGGING == 'true' then
+  vim.lsp.set_log_level(vim.log.levels.OFF)
+end
+if vim.env.NVIM_LSP_LOG_LEVEL then
+  vim.lsp.set_log_level(vim.env.NVIM_LSP_LOG_LEVEL)
+end
 
 vim.keymap.set('n', 'grl', vim.lsp.codelens.run, { desc = 'vim.lsp.codelens.run()' })
