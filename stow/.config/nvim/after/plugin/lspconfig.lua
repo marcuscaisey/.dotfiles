9aac691a2bf1aba7a0ddc2e514528423ca2dd202
Marcus Caisey <marcus@teckna.com>
2025-04-16 01:01:07 +0800

    clean up lspconfig config

--- stow/.config/nvim/after/plugin/lspconfig.lua
+++ stow/.config/nvim/after/plugin/lspconfig.lua
@@ -1,12 +1,1 @@
-local util = require('lspconfig.util')
-local ok, blink = pcall(require, 'blink.cmp')
-if not ok then
-  return
-end
-
-
-util.default_config = vim.tbl_deep_extend('force', util.default_config, {
-  capabilities = blink.get_lsp_capabilities(),
-})
-
 vim.keymap.set('n', '<Leader>rl', '<Cmd>LspRestart<CR>')
