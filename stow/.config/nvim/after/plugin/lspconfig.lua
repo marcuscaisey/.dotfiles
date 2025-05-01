8a0e8669217f22a2887e6fcd8f8d7f782ac33840
Marcus Caisey <marcus@teckna.com>
2025-05-01 17:30:26 +0800

    update LspRestart mapping

--- stow/.config/nvim/after/plugin/lspconfig.lua
+++ stow/.config/nvim/after/plugin/lspconfig.lua
@@ -1,1 +1,6 @@
-vim.keymap.set('n', '<Leader>rl', '<Cmd>LspRestart<CR>')
+vim.keymap.set('n', '<Leader>rl', function()
+  local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+  for _, client in ipairs(clients) do
+    vim.cmd.LspRestart(client.name)
+  end
+end)
