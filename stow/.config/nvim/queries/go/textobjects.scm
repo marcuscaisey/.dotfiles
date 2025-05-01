20d72202393570c83e40e993218c4a4ff32f3917
Marcus Caisey <marcus@teckna.com>
2025-05-01 12:03:10 +0800

    remove @value textobjects for array elements

--- stow/.config/nvim/queries/go/textobjects.scm
+++ stow/.config/nvim/queries/go/textobjects.scm
@@ -8,16 +8,6 @@
  (#make-range! "value.outer" @_start @_end)
 )
 
-(literal_value
-  "," @_start .
-  (literal_element) @value.inner
-  (#make-range! "value.outer" @_start @value.inner))
-
-(literal_value
-  . (literal_element) @value.inner
-  . ","? @_end
-  (#make-range! "value.outer" @value.inner @_end))
-
 right: (expression_list) @value.inner @value.outer
 
 (method_declaration) @method.outer
