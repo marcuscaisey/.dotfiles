3423866048d568c5a2d16b86679282336133c84f
Marcus Caisey <marcus@teckna.com>
2025-05-02 01:26:17 +0800

    only restart lsps with vim.lsp.config entries

--- stow/.config/nvim/after/plugin/lspconfig.lua
+++ stow/.config/nvim/after/plugin/lspconfig.lua
@@ -155,6 +155,8 @@ end, {
 vim.keymap.set('n', '<Leader>rl', function()
   local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
   for _, client in ipairs(clients) do
-    vim.cmd.LspRestart(client.name)
+    if vim.lsp.config[client.name] then
+      vim.cmd.LspRestart(client.name)
+    end
   end
 end)
