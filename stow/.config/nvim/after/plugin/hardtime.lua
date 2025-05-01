db081e1cf7d74ca14dfeb847e36810fc221ba4bc
Marcus Caisey <marcus@teckna.com>
2025-05-01 12:21:36 +0800

    exclude packer from hardtime

--- stow/.config/nvim/after/plugin/hardtime.lua
+++ stow/.config/nvim/after/plugin/hardtime.lua
@@ -2,7 +2,9 @@ local ok, hardtime = pcall(require, 'hardtime')
 if not ok then
   return
 end
+
 hardtime.setup({
+  disabled_filetypes = { 'qf', 'mason', 'oil', 'packer' },
   disabled_keys = {
     ['<Up>'] = {},
     ['<Down>'] = {},
