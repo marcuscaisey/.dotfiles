cf776902423a7108b89b4b3658155e51500f184f
Marcus Caisey <marcus@teckna.com>
2025-05-03 17:04:39 +0800

    tidy

--- stow/.config/nvim/after/plugin/blink.lua
+++ stow/.config/nvim/after/plugin/blink.lua
@@ -5,8 +5,8 @@ end
 
 blink.setup({
   keymap = {
-    ['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' },
-    ['<C-p>'] = { 'show', 'select_prev', 'fallback_to_mappings' },
+    ['<C-N>'] = { 'show', 'select_next', 'fallback_to_mappings' },
+    ['<C-P>'] = { 'show', 'select_prev', 'fallback_to_mappings' },
     ['<C-Space>'] = {},
   },
   completion = {
