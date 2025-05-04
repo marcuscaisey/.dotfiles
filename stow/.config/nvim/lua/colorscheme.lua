fe1c76e96c637c377aa562c0c936dd6073d9e683
Marcus Caisey <marcus@teckna.com>
2025-05-04 21:31:34 +0800

    remove copilot.vim theme config

--- stow/.config/nvim/lua/colorscheme.lua
+++ stow/.config/nvim/lua/colorscheme.lua
@@ -27,7 +27,6 @@ function M.setup()
       CurSearch = { link = 'IncSearch' },
     },
     integrations = {
-      copilot_vim = true,
       mason = true,
       ---@diagnostic disable-next-line: missing-fields
       native_lsp = {
