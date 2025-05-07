76c52dfd252571637602ea0fb0ff2441ac0d776a
Marcus Caisey <marcus@teckna.com>
2025-05-07 11:08:52 +0800

    add mappings to add to jumplist when jumping with count > 1

--- stow/.config/nvim/plugin/keymaps.lua
+++ stow/.config/nvim/plugin/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', '<C-W><', '<C-W>5<')
 vim.keymap.set('n', '<C-W>>', '<C-W>5>')
 vim.keymap.set('n', '<C-W>-', '<C-W>5-')
 vim.keymap.set('n', '<C-W>+', '<C-W>5+')
+
+vim.keymap.set('n', 'j', [[(v:count > 1 ? "m'" . v:count : "") . 'j']], { expr = true })
+vim.keymap.set('n', 'k', [[(v:count > 1 ? "m'" . v:count : "") . 'k']], { expr = true })
