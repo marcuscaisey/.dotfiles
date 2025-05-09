285dfb7c9ca0ceaebff3dc95077744ba8193e32b
Marcus Caisey <marcus@teckna.com>
2025-05-09 14:37:47 +0800

    Revert "remove n and N mappings"
    This reverts commit 9142378902e441e677c065430c9013ec2505609a.

--- stow/.config/nvim/plugin/keymaps.lua
+++ stow/.config/nvim/plugin/keymaps.lua
@@ -7,3 +7,6 @@ vim.keymap.set('n', '<C-W>+', '<C-W>5+')
 
 vim.keymap.set('n', 'j', [[(v:count > 1 ? "m'" . v:count : "") . 'j']], { expr = true })
 vim.keymap.set('n', 'k', [[(v:count > 1 ? "m'" . v:count : "") . 'k']], { expr = true })
+
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
--- stow/.config/nvim/plugin/nohlsearch.lua
+++ stow/.config/nvim/plugin/nohlsearch.lua
@@ -26,8 +26,24 @@ vim.api.nvim_create_autocmd('CursorHold', {
 })
 
 for _, key in ipairs({ 'n', 'N' }) do
+  local function rhs()
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), 'n', false)
+  end
+  for _, keymap in ipairs(vim.api.nvim_get_keymap('n')) do
+    if keymap.lhs == key then
+      if keymap.rhs then
+        function rhs()
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keymap.rhs, true, true, true), 'n', false)
+        end
+      elseif keymap.callback then
+        local callback = keymap.callback
+        ---@cast callback function
+        rhs = callback
+      end
+    end
+  end
   vim.keymap.set('n', key, function()
     schedule_nohlsearch()
-    return key
-  end, { expr = true, desc = string.format('schedule :nohlsearch after %dms, then %s', timeout_millis, key) })
+    rhs()
+  end, { desc = string.format('schedule :nohlsearch after %dms, then %s', timeout_millis, key) })
 end
