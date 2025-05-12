43a617c2f7c187219d30595a1615b5680bd2c157
Marcus Caisey <marcus@teckna.com>
2025-05-12 20:35:03 +0800

    fix / tidy go injections

--- stow/.config/nvim/queries/go/injections.scm
+++ stow/.config/nvim/queries/go/injections.scm
@@ -1,44 +1,20 @@
 ; extends
 
-; Inject SQL into strings which start with SELECT, INSERT, or UPDATE
-((raw_string_literal) @injection.content
-  (#lua-match? @injection.content "^`%s*SELECT")
-  (#offset! @injection.content 0 1 0 -1)
+; Inject sql into strings which start with SELECT, INSERT, or UPDATE
+(
+  [
+    (raw_string_literal_content)
+    (interpreted_string_literal_content)
+  ] @injection.content
+  (#match? @injection.content "^\\s*(SELECT|UPDATE|DELETE)")
   (#set! injection.language "sql"))
 
-((raw_string_literal) @injection.content
-  (#lua-match? @injection.content "^`%s*INSERT")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
-
-((raw_string_literal) @injection.content
-  (#lua-match? @injection.content "^`%s*UPDATE")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
-
-((interpreted_string_literal) @injection.content
-  (#lua-match? @injection.content "^\"%s*SELECT")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
-
-((interpreted_string_literal) @injection.content
-  (#lua-match? @injection.content "^\"%s*INSERT")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
-
-((interpreted_string_literal) @injection.content
-  (#lua-match? @injection.content "^\"%s*UPDATE")
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
-
-; Inject Python into strings which contain a line like "api = 4.0.0"
-((raw_string_literal) @injection.content
+; Inject python into raw strings which contain a line like "api = 4.0.0"
+((raw_string_literal_content) @injection.content
   (#match? @injection.content "api\\s*\\=\\s*(\"\\d\\.\\d\\d?\\.\\d\"|'\\d\\.\\d\\d?\\.\\d')\n")
-  (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "python"))
 
-; Inject Python into strings which contain a line like 'edge_api_version = "1"'
-((raw_string_literal) @injection.content
-  (#match? @injection.content "edge_api_version\\s*\\=\\s*\"\\d\"")
-  (#offset! @injection.content 0 1 0 -1)
+; Inject python into raw strings which contain a line like 'edge_api_version = "1"'
+((raw_string_literal_content) @injection.content
+  (#match? @injection.content "edge_api_version\\s*\\=\\s*\"\\d\"\n")
   (#set! injection.language "python"))
