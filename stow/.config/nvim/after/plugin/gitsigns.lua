1e9416ee34343b011f55c08bacd976e9df0aab72
Marcus Caisey <marcus@teckna.com>
2025-05-13 23:30:49 +0800

    delete gitsigns setqflist merging

--- stow/.config/nvim/after/plugin/gitsigns.lua
+++ stow/.config/nvim/after/plugin/gitsigns.lua
@@ -42,33 +42,7 @@ vim.keymap.set('n', '<Leader>gc', function()
       return
     end
 
-    -- Merge contiguous hunks
-    local contig_hunk_groups = {} ---@type {item:table, from:integer, to:integer}[][]
-    for _, item in ipairs(qflist) do
-      local from, to = item.text:match('^Lines (%d+)-(%d+).*$')
-      from, to = tonumber(from), tonumber(to)
-      local hunk = { item = item, from = from, to = to }
-      local cur_group = contig_hunk_groups[#contig_hunk_groups]
-      local cur_hunk = cur_group and cur_group[#cur_group]
-      if cur_hunk and item.bufnr == cur_hunk.item.bufnr and from - cur_hunk.to <= 1 then
-        table.insert(cur_group, hunk)
-      else
-        table.insert(contig_hunk_groups, { hunk })
-      end
-    end
-    local new_qflist = {}
-    for _, group in ipairs(contig_hunk_groups) do
-      local item = group[1].item
-      -- Attempting to stage a group of hunks from the first hunk where the first hunk only spans a single line fails
-      -- with "No hunk to stage". Therefore use the second hunk from the group as the quickfix item.
-      if #group > 1 and group[1].from == group[1].to then
-        item = group[2].item
-      end
-      item.text = string.format('Lines %s-%s', group[1].from, group[#group].to)
-      table.insert(new_qflist, item)
-    end
-
-    vim.fn.setqflist(new_qflist)
+    vim.fn.setqflist(qflist)
     vim.cmd.copen()
     vim.cmd.cfirst()
   end)
