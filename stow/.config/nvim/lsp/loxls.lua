2ef8eaf5af287c1c9d0e3272368779a4e1d2bc3b
Marcus Caisey <marcus@teckna.com>
2025-09-18 08:48:33 +0100

    start loxls from lox dir

--- stow/.config/nvim/lsp/loxls.lua
+++ stow/.config/nvim/lsp/loxls.lua
@@ -1,5 +1,6 @@
 ---@type vim.lsp.Config
 return {
   cmd = { 'go', 'run', 'github.com/marcuscaisey/lox/loxls' },
+  cmd_cwd = vim.fs.joinpath(vim.env.HOME, 'scratch', 'lox'),
   filetypes = { 'lox' },
 }
