1a7dbfe61094ed4c3afa207a862b68a2aabc658b
Marcus Caisey <marcus@teckna.com>
2025-10-02 09:06:39 +0100

    execute zz after all quickfix mappings

--- stow/.config/nvim/plugin/keymaps.lua
+++ stow/.config/nvim/plugin/keymaps.lua
@@ -11,4 +11,24 @@ vim.keymap.set('n', 'k', [[(v:count > 1 ? "m'" . v:count : "") . 'k']], { expr =
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
 
+local quickfix_loclist_mappings = { '[q', ']q', '[Q', ']Q', '[<C-Q>', ']<C-Q>', '[l', ']l', '[l', ']l', '[<C-L>', ']<C-L>' }
+for _, keymap in ipairs(vim.api.nvim_get_keymap('n')) do
+  if vim.list_contains(quickfix_loclist_mappings, keymap.lhs) then
+    local rhs ---@type function?
+    if keymap.rhs then
+      function rhs()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keymap.rhs, true, true, true), 'n', false)
+      end
+    elseif keymap.callback then
+      rhs = keymap.callback
+    end
+    if rhs then
+      vim.keymap.set('n', keymap.lhs, function()
+        rhs()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('zz', true, true, true), 'n', false)
+      end, { desc = string.format('%s + zz', keymap.desc) })
+    end
+  end
+end
+
 vim.keymap.set('n', '<leader>re', '<Cmd>mksession! /tmp/session.vim | restart +wqa source /tmp/session.vim<CR>')
