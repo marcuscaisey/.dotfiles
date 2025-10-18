a3352ec7ae25a89d575b2f50b9092df08c1c62dc
Marcus Caisey <marcus@teckna.com>
2025-10-18 12:51:35 +0100

    simplify nohlsearch mapping wrapping

--- stow/.config/nvim/plugin/nohlsearch.lua
+++ stow/.config/nvim/plugin/nohlsearch.lua
@@ -26,9 +26,7 @@ vim.api.nvim_create_autocmd('CursorHold', {
 })
 
 for _, key in ipairs({ 'n', 'N' }) do
-  local function rhs()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), 'n', false)
-  end
+  local rhs ---@type function?
   for _, keymap in ipairs(vim.api.nvim_get_keymap('n')) do
     if keymap.lhs == key then
       if keymap.rhs then
@@ -36,14 +34,14 @@ for _, key in ipairs({ 'n', 'N' }) do
           vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keymap.rhs, true, true, true), 'n', false)
         end
       elseif keymap.callback then
-        local callback = keymap.callback
-        ---@cast callback function
-        rhs = callback
+        rhs = keymap.callback
       end
     end
   end
-  vim.keymap.set('n', key, function()
-    schedule_nohlsearch()
-    rhs()
-  end, { desc = string.format('schedule :nohlsearch after %dms, then %s', timeout_millis, key) })
+  if rhs then
+    vim.keymap.set('n', key, function()
+      schedule_nohlsearch()
+      rhs()
+    end, { desc = string.format('schedule :nohlsearch after %dms, then %s', timeout_millis, key) })
+  end
 end
