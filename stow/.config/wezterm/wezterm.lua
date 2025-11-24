d0ac70d711d9cada95f4c31cea90beb3d0109330
Marcus Caisey <marcus@teckna.com>
2025-11-24 16:14:31 +0000

    add please build label to wezterm quickselect patterns

--- stow/.config/wezterm/wezterm.lua
+++ stow/.config/wezterm/wezterm.lua
@@ -20,6 +20,10 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.audible_bell = 'Disabled'
 
+config.quick_select_patterns = {
+  -- Please build label
+  [[//\w+(?:/\w+)*:[\w#]+]],
+}
 config.quick_select_remove_styling = true
 
 return config
