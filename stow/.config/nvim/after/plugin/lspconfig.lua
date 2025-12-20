7ca573afe707c7437f0a7795ef8362dd35217ccb
Marcus Caisey <marcus@teckna.com>
2025-12-20 10:15:59 +0000

    remove <Leader>rl command

--- stow/.config/nvim/after/plugin/lspconfig.lua
+++ stow/.config/nvim/after/plugin/lspconfig.lua
@@ -1,12 +1,3 @@
-vim.keymap.set('n', '<Leader>rl', function()
-  local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
-  for _, client in ipairs(clients) do
-    if vim.lsp.config[client.name] then
-      vim.cmd.LspRestart(client.name)
-    end
-  end
-end)
-
 vim.api.nvim_create_user_command('LspLog', function()
   vim.cmd(string.format('tabnew %s', vim.lsp.log.get_filename()))
 end, { desc = 'Opens the Nvim LSP client log.' })
