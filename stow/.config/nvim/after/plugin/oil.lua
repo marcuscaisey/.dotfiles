3c29627c65b0e75c756d35ebb101bf3cc2151425
Marcus Caisey <marcus@teckna.com>
2025-12-31 11:55:43 +0000

    add oil command to open in git root

--- stow/.config/nvim/after/plugin/oil.lua
+++ stow/.config/nvim/after/plugin/oil.lua
@@ -26,6 +26,16 @@ oil.setup({
       desc = 'Grep over files in the current directory',
     },
     ['_'] = 'actions.open_cwd',
+    ['+'] = {
+      desc = "Open oil in the git repository root",
+      callback = function()
+        local root = vim.fs.root(oil.get_current_dir() or '', '.git')
+        if not root then
+          return
+        end
+        oil.open(root)
+      end,
+    },
     ['~'] = { 'actions.cd', opts = { scope = 'win' }, desc = ':lcd to the current oil directory' },
   },
   view_options = {
