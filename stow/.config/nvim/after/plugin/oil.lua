a3097646b061137af1ebcef75b597c1b493be5bb
Marcus Caisey <marcus@teckna.com>
2025-12-31 11:46:23 +0000

    remove redundant oil mappings

--- stow/.config/nvim/after/plugin/oil.lua
+++ stow/.config/nvim/after/plugin/oil.lua
@@ -8,22 +8,15 @@ if not ok then
 end
 
 oil.setup({
-  use_default_keymaps = false,
   keymaps = {
-    ['g?'] = 'actions.show_help',
-    ['<CR>'] = 'actions.select',
-    ['<C-S>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
-    ['<C-H>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
     ['gp'] = 'actions.preview',
-    ['<C-P>'] = {
+    ['<C-p>'] = {
       function()
         fzf.files({
           fd_opts = fzf.defaults.files.fd_opts .. ' --type d',
           cwd = oil.get_current_dir(),
         })
       end,
-      mode = 'n',
-      nowait = true,
       desc = 'Find files in the current directory',
     },
     ['<C-G>'] = {
@@ -32,13 +25,8 @@ oil.setup({
       end,
       desc = 'Grep over files in the current directory',
     },
-    ['<C-L>'] = 'actions.refresh',
-    ['-'] = 'actions.parent',
     ['_'] = 'actions.open_cwd',
-    ['`'] = 'actions.cd',
     ['~'] = { 'actions.cd', opts = { scope = 'win' }, desc = ':lcd to the current oil directory' },
-    ['gs'] = 'actions.change_sort',
-    ['g.'] = 'actions.toggle_hidden',
   },
   view_options = {
     -- Show files and directories that start with "."
