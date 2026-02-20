2fdd7dc0889ebe24d57cfe2c678b6d96ea25cb87
Marcus Caisey <marcus@teckna.com>
2026-02-20 10:39:18 +0000

    print args after adding

--- stow/.config/nvim/plugin/args.lua
+++ stow/.config/nvim/plugin/args.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<Leader>aa', '<Cmd>argadd | argdedupe<CR>')
+vim.keymap.set('n', '<Leader>aa', '<Cmd>argadd | argdedupe | args<CR>')
 vim.keymap.set('n', '<Leader>ac', '<Cmd>argdelete *<CR>')
 for i = 0, 9 do
   vim.keymap.set('n', string.format('<Leader>%d', i), string.format('<Cmd>argument %d<CR>', i))
