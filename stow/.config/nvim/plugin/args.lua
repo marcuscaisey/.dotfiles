5d6d856fa60f924e50ed61d3bcba787e7e111e3f
Marcus Caisey <marcus@teckna.com>
2026-03-01 14:57:38 +0000

    add args mapping

--- stow/.config/nvim/plugin/args.lua
+++ stow/.config/nvim/plugin/args.lua
@@ -1,4 +1,5 @@
 vim.keymap.set('n', '<Leader>aa', '<Cmd>$arge % | argdedupe | args<CR>')
+vim.keymap.set('n', '<Leader>as', '<Cmd>args<CR>')
 vim.keymap.set('n', '<Leader>ac', '<Cmd>argdelete * | args<CR>')
 for i = 0, 9 do
   vim.keymap.set('n', string.format('<Leader>%d', i), string.format('<Cmd>argument %d | args<CR>', i))
