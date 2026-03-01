c73930405939bb441697c3e7b776e4224bac1ccb
Marcus Caisey <marcus@teckna.com>
2026-03-01 18:33:20 +0000

    simplify gitsigns config

--- stow/.config/nvim/after/plugin/gitsigns.lua
+++ stow/.config/nvim/after/plugin/gitsigns.lua
@@ -2,55 +2,35 @@ local ok, gitsigns = pcall(require, 'gitsigns')
 if not ok then
   return
 end
-local actions = require('gitsigns.actions')
-
----@type {name:string, hl_group:string, symbol:string}[]
-local status_sections = {
-  { name = 'added', hl_group = 'diffAdded', symbol = '+' },
-  { name = 'changed', hl_group = 'diffChanged', symbol = '~' },
-  { name = 'removed', hl_group = 'diffRemoved', symbol = '-' },
-}
 
 gitsigns.setup({
   numhl = true,
   attach_to_untracked = true,
-  ---@param status table<string,any>
+  ---@param status {added:integer, changed:integer, removed:integer}
   ---@return string
   status_formatter = function(status)
+    local added, changed, removed = status.added, status.changed, status.removed
     local status_txt = {}
-    for _, section in ipairs(status_sections) do
-      local count = status[section.name]
-      if count and count > 0 then
-        table.insert(status_txt, '%#' .. section.hl_group .. '#' .. section.symbol .. count)
-      end
+    if added and added > 0 then
+      table.insert(status_txt, '%#GitSignsAdd#+' .. added)
+    end
+    if changed and changed > 0 then
+      table.insert(status_txt, '%#GitSignsChange#~' .. changed)
+    end
+    if removed and removed > 0 then
+      table.insert(status_txt, '%#GitSignsDelete#-' .. removed)
     end
     return table.concat(status_txt, ' ')
   end,
 })
 
-vim.keymap.set('n', ']c', function()
-  if vim.wo.diff then
-    vim.cmd.normal({ ']c', bang = true })
-  end
-  actions.nav_hunk('next')
-end, { desc = 'gitsigns.actions.nav_hunk("next")' })
-vim.keymap.set('n', '[c', function()
-  if vim.wo.diff then
-    vim.cmd.normal({ '[c', bang = true })
-  end
-  actions.nav_hunk('prev')
-end, { desc = 'gitsigns.actions.nav_hunk("prev")' })
-vim.keymap.set('n', '<Leader>hs', actions.stage_hunk, { desc = 'gitsigns.actions.stage_hunk()' })
-vim.keymap.set('v', '<Leader>hs', function()
-  actions.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-end, { desc = 'gitsigns.actions.stage_hunk()' })
-vim.keymap.set('n', '<Leader>hS', actions.stage_buffer, { desc = 'gitsigns.actions.stage_buffer()' })
-vim.keymap.set('n', '<Leader>hr', actions.reset_hunk, { desc = 'gitsigns.actions.revert_hunk()' })
-vim.keymap.set('v', '<Leader>hr', function()
-  actions.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-end, { desc = 'gitsigns.actions.reset_hunk()' })
-vim.keymap.set('n', '<Leader>hR', actions.reset_buffer, { desc = 'gitsigns.actions.reset_buffer()' })
-vim.keymap.set('n', '<Leader>hp', actions.preview_hunk_inline, { desc = 'gitsigns.actions.preview_hunk_inline()' })
+vim.keymap.set('n', ']c', '<Cmd>execute &diff ? "normal! ]c" : "Gitsigns nav_hunk next"<CR>')
+vim.keymap.set('n', '[c', '<Cmd>execute &diff ? "normal! ]c" : "Gitsigns nav_hunk prev"<CR>')
+vim.keymap.set({ 'n', 'v' }, '<Leader>hs', '<Cmd>Gitsigns stage_hunk<CR>')
+vim.keymap.set('n', '<Leader>hS', '<Cmd>Gitsigns stage_buffer<CR>')
+vim.keymap.set({ 'n', 'v' }, '<Leader>hr', '<Cmd>Gitsigns reset_hunk<CR>')
+vim.keymap.set('n', '<Leader>hR', '<Cmd>Gitsigns reset_buffer<CR>')
+vim.keymap.set('n', '<Leader>hp', '<Cmd>Gitsigns preview_hunk_inline<CR>')
 vim.keymap.set('n', '<Leader>gc', function()
   ---@diagnostic disable-next-line: redundant-parameter
   gitsigns.setqflist('all', { open = false }, function()
