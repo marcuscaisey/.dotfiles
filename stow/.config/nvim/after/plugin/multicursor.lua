101e58d68544726d44253ab4319aaa604b2f5078
Marcus Caisey <marcus@teckna.com>
2026-03-08 14:51:56 +0000

    remove multicursor highlight overrides

--- stow/.config/nvim/after/plugin/multicursor.lua
+++ stow/.config/nvim/after/plugin/multicursor.lua
@@ -5,13 +5,6 @@ end
 
 multicursor.setup()
 
-vim.api.nvim_set_hl(0, 'MultiCursorCursor', { link = 'Cursor' })
-vim.api.nvim_set_hl(0, 'MultiCursorVisual', { link = 'Visual' })
-vim.api.nvim_set_hl(0, 'MultiCursorSign', { link = 'SignColumn' })
-vim.api.nvim_set_hl(0, 'MultiCursorDisabledCursor', { link = 'Visual' })
-vim.api.nvim_set_hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
-vim.api.nvim_set_hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
-
 vim.keymap.set({ 'n', 'v' }, '<Up>', '<Cmd>lua require("multicursor-nvim").addCursor("k")<CR>')
 vim.keymap.set({ 'n', 'v' }, '<Down>', '<Cmd>lua require("multicursor-nvim").addCursor("j")<CR>')
 vim.keymap.set({ 'n', 'v' }, '<C-N>', '<Cmd>lua require("multicursor-nvim").addCursor("*")<CR>')
