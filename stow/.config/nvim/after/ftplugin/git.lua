1cf582663e2ecdf2fd99451dc06b97129e023620
Marcus Caisey <marcus@teckna.com>
2026-03-08 14:38:02 +0000

    remove fugitive commit hash yank mapping

--- stow/.config/nvim/after/ftplugin/git.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set('n', '<Leader>yy', function()
-  local filepath = vim.api.nvim_buf_get_name(0)
-  local hash = filepath:match('.+/%.git//(.+)')
-  vim.fn.setreg('"', hash)
-  vim.fn.setreg('*', hash)
-  print(string.format('Yanked %s', hash))
-end, { desc = 'Yank the hash of the commit patch', buffer = true })
