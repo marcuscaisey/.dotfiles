41e4527e16af71f34ff6506e129b9e006f9e434b
Marcus Caisey <marcus@teckna.com>
2026-03-09 12:40:50 +0000

    remove @call.inner ts text object mapping

--- stow/.config/nvim/after/plugin/treesitter_textobjects.lua
+++ stow/.config/nvim/after/plugin/treesitter_textobjects.lua
@@ -22,7 +22,6 @@ vim.keymap.set({ 'x', 'o' }, 'ia', '<Cmd>lua require("nvim-treesitter-textobject
 vim.keymap.set({ 'x', 'o' }, 'aa', '<Cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")<CR>')
 vim.keymap.set({ 'x', 'o' }, ']a', '<Cmd>lua require("nvim-treesitter-textobjects.move").goto_next_start("@parameter.inner", "textobjects")<CR>')
 vim.keymap.set({ 'x', 'o' }, '[a', '<Cmd>lua require("nvim-treesitter-textobjects.move").goto_previous_start("@parameter.inner", "textobjects")<CR>')
-vim.keymap.set({ 'x', 'o' }, 'ic', '<Cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@call.inner", "textobjects")<CR>')
 vim.keymap.set({ 'x', 'o' }, 'ac', '<Cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@call.outer", "textobjects")<CR>')
 vim.keymap.set({ 'x', 'o' }, 'ib', '<Cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@block.inner", "textobjects")<CR>')
 vim.keymap.set({ 'x', 'o' }, 'ab', '<Cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@block.outer", "textobjects")<CR>')
