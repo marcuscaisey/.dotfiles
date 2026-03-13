32fe0d0747f7e9c177ffad77ddb6800f157a7636
Marcus Caisey <marcus@teckna.com>
2026-03-13 10:26:14 +0000

    simplify some autocmd callbacks

--- stow/.config/nvim/plugin/options.lua
+++ stow/.config/nvim/plugin/options.lua
@@ -22,26 +22,18 @@ local augroup = vim.api.nvim_create_augroup('options_auto_relativenumber', {})
 vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter' }, {
     group = augroup,
     desc = 'Set relativenumber in focused window when not in insert mode',
-    callback = function()
-        if vim.api.nvim_get_mode().mode ~= 'i' and vim.wo.number then
-            vim.wo.relativenumber = true
-        end
-    end,
+    command = "if mode() != 'i' && &number | setlocal relativenumber | endif",
 })
 vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave' }, {
     group = augroup,
     desc = 'Unset relativenumber in unfocused windows or when in insert mode',
-    callback = function()
-        vim.wo.relativenumber = false
-    end,
+    command = 'set norelativenumber',
 })
 
 vim.api.nvim_create_autocmd('FileType', {
     group = vim.api.nvim_create_augroup('remove_formatoptions', {}),
     desc = 'Remove c, r, and o from formatoptions after any ftplugin may have modified them',
-    callback = function()
-        vim.cmd('set formatoptions-=cro')
-    end,
+    command = 'set formatoptions-=cro',
 })
 
 vim.keymap.set('n', 'yow', '<Cmd>setlocal wrap!<CR>')
--- stow/.config/nvim/plugin/tmux.lua
+++ stow/.config/nvim/plugin/tmux.lua
@@ -1,21 +1,13 @@
 local augroup = vim.api.nvim_create_augroup('tmux_window_renaming', { clear = true })
 
-vim.api.nvim_create_autocmd({ 'VimLeave' }, {
-    callback = function()
-        if os.getenv('TMUX') then
-            vim.system({ 'tmux', 'set-window-option', 'automatic-rename', 'on' })
-        end
-    end,
+vim.api.nvim_create_autocmd('VimLeave', {
     group = augroup,
     desc = 'Turn tmux automatic window renaming on',
+    command = "if !empty($TMUX) | call system(['tmux', 'set-window-option', 'automatic-rename', 'on']) | endif",
 })
 
-vim.api.nvim_create_autocmd({ 'DirChanged' }, {
-    callback = function()
-        if os.getenv('TMUX') then
-            vim.system({ 'tmux', 'rename-window', vim.fs.basename(vim.v.event.cwd) .. ':nvim' })
-        end
-    end,
+vim.api.nvim_create_autocmd('DirChanged', {
     group = augroup,
     desc = 'Rename the tmux window to $cwd:nvim',
+    command = "if !empty($TMUX) | call system(['tmux', 'rename-window', fnamemodify(v:event.cwd, ':t') . ':nvim']) | endif",
 })
