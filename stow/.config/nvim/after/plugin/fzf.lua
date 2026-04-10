bc6c7abc49430a75d52de4942d3ef778b84177db
Marcus Caisey <marcus@teckna.com>
2026-04-10 23:44:03 +0100

    fix g<C-]> mapping

--- stow/.config/nvim/after/plugin/fzf.lua
+++ stow/.config/nvim/after/plugin/fzf.lua
@@ -100,7 +100,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
         if client:supports_method('textDocument/definition') then
             vim.keymap.set('n', 'g]', '<Cmd>FzfLua lsp_definitions<CR>', { buffer = 0, desc = 'Jump to definition (always show UI)' })
-            vim.keymap.set('n', 'g<C-]>', '<Cmd>FzfLua lsp_definitions jump1<CR>', { buffer = 0, desc = 'Jump to definition (show UI if multiple)' })
+            vim.keymap.set('n', 'g<C-]>', '<Cmd>FzfLua lsp_definitions jump1=true<CR>', { buffer = 0, desc = 'Jump to definition (show UI if many)' })
         end
     end,
 })
