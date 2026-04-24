c15ed1803aaca7b0d0617b0d624ae2a86b52cd47
Marcus Caisey <marcus@teckna.com>
2026-04-24 08:56:37 +0100

    remove redundant wezterm font setting

--- stow/.config/wezterm/wezterm.lua
+++ stow/.config/wezterm/wezterm.lua
@@ -4,7 +4,6 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
 
-config.font = wezterm.font('JetBrains Mono')
 config.font_size = 14
 
 config.hide_tab_bar_if_only_one_tab = true
