7c7aedbb635bcdd1cc43a5abbc469292d0871537
Marcus Caisey <marcus@teckna.com>
2026-04-24 16:25:50 +0100

    fixup! only trigger autocomplete when a character has been typed

--- stow/.config/nvim/plugin/cmdline.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd('CmdlineChanged', {
-    desc = 'Trigger wildcard expansion',
-    group = vim.api.nvim_create_augroup('cmdline.trigger_wildcard_expansion', {}),
-    pattern = { ':', '/', '?' },
-    command = 'call wildtrigger()',
-})
