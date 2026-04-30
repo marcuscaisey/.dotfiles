d3143534f63626ddfc9e0789b1efa54364e4cfbe
Marcus Caisey <marcus@teckna.com>
2026-05-01 00:31:36 +0100

    update please config

--- stow/.config/nvim/after/plugin/please.lua
+++ stow/.config/nvim/after/plugin/please.lua
@@ -3,16 +3,21 @@ if not ok then
     return
 end
 
-please.setup({ puku_command = { 'puku' } })
+please.setup({
+    formatting = { puku_command = { 'puku' } },
+})
 
 vim.keymap.set('n', '<leader>pb', '<Cmd>Please build<CR>', { desc = 'Build please target' })
 vim.keymap.set('n', '<leader>pr', '<Cmd>Please run<CR>', { desc = 'Run please target' })
 vim.keymap.set('n', '<leader>pt', '<Cmd>Please test<CR>', { desc = 'Test please target' })
-vim.keymap.set('n', '<leader>pct', '<Cmd>Please test under_cursor<CR>', { desc = 'Test please target (test under cursor)' })
+vim.keymap.set('n', '<leader>pT', '<Cmd>Please test under_cursor<CR>', { desc = 'Test please target (test under cursor)' })
+vim.keymap.set('n', '<leader>pc', '<Cmd>Please cover quickfix<CR>', { desc = 'Cover please target' })
+vim.keymap.set('n', '<leader>pC', '<Cmd>Please cover quickfix under_cursor<CR>', { desc = 'Cover please target (test under cursor)' })
+vim.keymap.set('n', '<leader>pv', '<Cmd>Please toggle_coverage_highlighting<CR>', { desc = 'Toggle please coverage highlighting' })
 vim.keymap.set('n', '<leader>pd', '<Cmd>Please debug<CR>', { desc = 'Debug please target' })
-vim.keymap.set('n', '<leader>pcd', '<Cmd>Please debug under_cursor<CR>', { desc = 'Debug please target (test under cursor)' })
+vim.keymap.set('n', '<leader>pD', '<Cmd>Please debug under_cursor<CR>', { desc = 'Debug please target (test under cursor)' })
 vim.keymap.set('n', '<leader>ph', '<Cmd>Please history<CR>', { desc = 'List please command history' })
-vim.keymap.set('n', '<leader>pch', '<Cmd>Please clear_history<CR>', { desc = 'Clear please command history' })
+vim.keymap.set('n', '<leader>pH', '<Cmd>Please clear_history<CR>', { desc = 'Clear please command history' })
 vim.keymap.set('n', '<leader>pp', '<Cmd>Please set_profile<CR>', { desc = 'Set please profile' })
 vim.keymap.set('n', '<leader>pm', '<Cmd>Please maximise_popup<CR>', { desc = 'Maximise please popup' })
 vim.keymap.set('n', '<leader>pj', '<Cmd>Please jump_to_target<CR>', { desc = 'Jump to please target' })
