68f587e0597f9e5a60f29fa3a27cc5dc72f5a7d7
Marcus Caisey <marcus@teckna.com>
2026-05-01 22:17:12 +0100

    remove autocomplete enable autocmd

--- stow/.config/nvim/plugin/autocomplete.lua
+++ stow/.config/nvim/plugin/autocomplete.lua
@@ -1,23 +1,3 @@
-vim.api.nvim_create_autocmd('InsertCharPre', {
-    desc = 'Enable autocomplete and trigger completion if a non-space character is typed',
-    group = vim.api.nvim_create_augroup('autocomplete.enable_autocomplete', {}),
-    callback = function()
-        if not vim.o.autocomplete and not vim.v.char:match('%s') then
-            vim.o.autocomplete = true
-            if vim.fn.pumvisible() ~= 1 then
-                local key = vim.api.nvim_replace_termcodes('<C-n>', true, false, true)
-                vim.api.nvim_feedkeys(key, 'n', false)
-            end
-        end
-    end,
-})
-
-vim.api.nvim_create_autocmd('InsertLeave', {
-    desc = 'Disable autocomplete',
-    group = vim.api.nvim_create_augroup('autocomplete.disable_autocomplete', {}),
-    command = 'set noautocomplete',
-})
-
 vim.api.nvim_create_autocmd('CmdlineChanged', {
     desc = 'Trigger command line completion',
     group = vim.api.nvim_create_augroup('autocomplete.trigger_cmdline_autocomplete', {}),
