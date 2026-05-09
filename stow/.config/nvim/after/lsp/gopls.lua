12ab3d8eace0d1fd0aab048aee6596902ade1c34
Marcus Caisey <marcus@teckna.com>
2026-05-09 12:09:22 +0100

    Revert "move gopls organizeImports autocmd to go file"
    This reverts commit c74908e39ac7394570ca625ea2e9fa8310533dd8.

--- stow/.config/nvim/after/lsp/gopls.lua
+++ stow/.config/nvim/after/lsp/gopls.lua
@@ -6,7 +6,26 @@ return {
             semanticTokenTypes = { string = false },
         },
     },
-    on_attach = function(client)
+    on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            group = vim.api.nvim_create_augroup('gopls.organize_imports_' .. bufnr, {}),
+            buffer = bufnr,
+            desc = 'Run gopls source.organizeImports code action',
+            callback = function()
+                local params = vim.lsp.util.make_range_params(0, client.offset_encoding) ---@type lsp.CodeActionParams
+                params.context = { only = { 'source.organizeImports' }, diagnostics = {} }
+                client:request('textDocument/codeAction', params, function(err, result)
+                    if err or not result then
+                        return
+                    end
+                    for _, code_action in pairs(result) do
+                        if code_action.edit then
+                            vim.lsp.util.apply_workspace_edit(code_action.edit, client.offset_encoding)
+                        end
+                    end
+                end, bufnr)
+            end,
+        })
         -- This doesn't work very well sometimes.
         client.server_capabilities.semanticTokensProvider.range = false
     end,
