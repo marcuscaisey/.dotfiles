4d752be4883ced08f7d3bef3e91772ab31f45629
Marcus Caisey <marcus@teckna.com>
2026-06-05 10:42:42 +0100

    restore viewport after gq

--- stow/.config/nvim/plugin/formatting.lua
+++ stow/.config/nvim/plugin/formatting.lua
@@ -1,12 +1,12 @@
 vim.keymap.set('n', 'gq', function()
-    local cursor = vim.api.nvim_win_get_cursor(0)
+    local view = vim.fn.winsaveview()
     vim.api.nvim_create_autocmd('CursorMoved', {
         group = vim.api.nvim_create_augroup('formatting.restore_cursor', {}),
         buf = 0,
         once = true,
         desc = 'Restore cursor position after formatting',
         callback = function()
-            vim.api.nvim_win_set_cursor(0, cursor)
+            vim.fn.winrestview(view)
         end,
     })
     return 'gq'
