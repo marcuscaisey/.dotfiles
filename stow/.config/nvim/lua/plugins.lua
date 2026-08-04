e923f6f7437473440ef67a6b42af3f92d26eaf2a
Marcus Caisey <marcus@teckna.com>
2026-08-04 23:22:16 +0100

    sort plugin list

--- stow/.config/nvim/lua/plugins.lua
+++ stow/.config/nvim/lua/plugins.lua
@@ -16,6 +16,7 @@ vim.api.nvim_create_autocmd('PackChanged', {
 })
 
 vim.pack.add({
+    { src = 'https://github.com/barrettruth/canola.nvim' },
     { src = 'https://github.com/bkad/camelcasemotion' },
     { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
     { src = 'https://github.com/ibhagwan/fzf-lua' },
@@ -37,7 +38,6 @@ vim.pack.add({
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter-context' },
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects' },
-    { src = 'https://github.com/barrettruth/canola.nvim' },
 })
 -- Set so that ReplaceWithRegister doesn't create default mappings before we can override them in
 -- after/plugin/replacewithregister.lua
