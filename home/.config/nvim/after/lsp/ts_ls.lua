bdcb63e7713ee8c0718a7228f4dcc2635211126b
Marcus Caisey <marcus@teckna.com>
2026-08-19 18:11:07 +0100

    disable all types of formatting from ts_ls

--- home/.config/nvim/after/lsp/ts_ls.lua
+++ home/.config/nvim/after/lsp/ts_ls.lua
@@ -1,6 +1,8 @@
 ---@type vim.lsp.Config
 return {
     on_attach = function(client)
-        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentFormattingProvider = nil
+        client.server_capabilities.documentRangeFormattingProvider = nil
+        client.server_capabilities.documentOnTypeFormattingProvider = nil
     end,
 }
